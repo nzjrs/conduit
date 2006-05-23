b934487e2e8703f229fbcc4caa13d243470ba983
John Stowers <jstowers@src.gnome.org>
2006-05-23 10:04:44 +0000

    moving things around to match the deskbar layout
    svn path=/trunk/; revision=13

--- src/MyBox.py
+++ src/MyBox.py
@@ -1,28 +1,28 @@
-import diacanvas as dia
+import diacanvas
 import gobject
 import time
 
 # A custom widget, Clock
-class MyBox(dia.CanvasElement, dia.CanvasGroupable):
+class MyBox(diacanvas.CanvasElement, diacanvas.CanvasGroupable):
     def __init__(self):
         self.__gobject_init__()
-        #dia.CanvasBox.__init__(self)
-        #dia.CanvasGroupable.__init__(self)
+        #diacanvas.CanvasBox.__init__(self)
+        #diacanvas.CanvasGroupable.__init__(self)
         #contains a ellipse, and hour minute second hands
-        self.ellipse = dia.shape.Ellipse()
-        self.line = dia.shape.Path()
-        #self.box = dia.shape.Box()
+        self.ellipse = diacanvas.shape.Ellipse()
+        self.line = diacanvas.shape.Path()
+        #self.box = diacanvas.shape.Box()
 
         #make the clock tick
         self.timeout_handler_id = gobject.timeout_add(1000, self.timer_handler)
         self.tick = 20
 
     def on_update (self,  affine):
-        dia.CanvasElement.on_update (self, affine)
+        diacanvas.CanvasElement.on_update (self, affine)
         self.ellipse.request_update() # request update, due to rotation
 
     def on_shape_iter (self):
-        #for s in dia.CanvasBox.on_shape_iter(self):
+        #for s in diacanvas.CanvasBox.on_shape_iter(self):
         #    yield s
         yield self.ellipse
         yield self.line
@@ -31,19 +31,19 @@ class MyBox(dia.CanvasElement, dia.CanvasGroupable):
         """do nothing with this callback, it is used to determine the distance
         between the (mouse) cursor and the item. You may also ommit this
         callback, do the parent will automatically be called."""
-        return dia.CanvasElement.on_point(self, x, y)
+        return diacanvas.CanvasElement.on_point(self, x, y)
 
     def on_move (self, x, y, interactive):
         """The item is moved. Usually you don't have to override this one."""
         print 'move'
-        return dia.CanvasElement.on_move(self, x, y, interactive);
+        return diacanvas.CanvasElement.on_move(self, x, y, interactive);
 
     def on_handle_motion (self, handle, wx, wy, mask):
         """One of the item's handles has been moved."""
-        return dia.CanvasElement.on_handle_motion(self, handle, wx, wy, mask);
+        return diacanvas.CanvasElement.on_handle_motion(self, handle, wx, wy, mask);
 
     def on_glue (self, handle, wx, wy):
-        ret = dia.CanvasElement.on_glue(self, handle, wx, wy);
+        ret = diacanvas.CanvasElement.on_glue(self, handle, wx, wy);
         return ret
 
 
@@ -63,8 +63,212 @@ class MyBox(dia.CanvasElement, dia.CanvasGroupable):
 
 # Create a GObject type for this item:
 gobject.type_register(MyBox) 
-# Set DiaCanvasItem specific callbacks for the GObject side of this class
-# This only has to be done the first time you create a Python class based
-# on a diacanvas.CanvasItem (DiaCanvasItem).
-dia.set_callbacks(MyBox)
-dia.set_groupable(MyBox)
+diacanvas.set_callbacks(MyBox)
+diacanvas.set_groupable(MyBox)
+
+import diacanvas.shape as shape
+class MyBox2(diacanvas.CanvasElement, diacanvas.CanvasAbstractGroup):
+    def __init__(self):
+        self.__gobject_init__()
+
+        # create our line shapes (a red and a green line)
+        self.top_line = shape.Path()
+        self.top_line.set_color(diacanvas.color(255, 0, 0))
+        self.top_line.set_line_width(2)
+        self.bottom_line = shape.Path()
+        self.bottom_line.set_color(diacanvas.color(0, 255, 0))
+        self.bottom_line.set_line_width(2)
+        
+        # create a text object (CanvasText is a composite object)
+        self.text = diacanvas.CanvasText()
+        self.text.set(text="hello")
+        # make the text a child of this canvas item
+        self.text.set_child_of(self)
+        #self.add_construction(self.text)
+        
+    def on_update(self, affine):
+        # create a line on the top
+        # (line() takes one argument: a list of points)
+        self.top_line.line([(0, 0), (self.width, 0)])
+
+        # create a line on the bottom
+        self.bottom_line.line([(0, self.height), (self.width, self.height)])
+
+        # give the text the same width and height as out object
+        self.text.set(width=self.width, height=self.height)
+        
+        # update the text
+        self.update_child(self.text, affine)
+
+        # update the parent
+        diacanvas.CanvasElement.on_update(self, affine)
+
+        # expand the boundries of this canvas item so the lines (with
+        # width 2.0) are inside the boundries of this canvas item.
+        self.expand_bounds(1.0)
+
+    def on_shape_iter(self):
+        # an iterator 
+        yield self.top_line
+        yield self.bottom_line
+        # alternative:
+        #return iter([self.top_line, self.bottom_line])
+
+    def on_event(self, event):
+        # make sure key events are send to our text object
+        if event.type == diacanvas.EVENT_KEY_PRESS:
+            print 'key'
+            self.text.focus()
+            return self.text.on_event(event)
+        else:
+            return diacanvas.CanvasElement.on_event(self, event)
+
+    # Groupable
+
+    def on_groupable_add(self, item):
+        """Add a new item. This is not allowed in this case.
+        """
+        return 0
+
+    def on_groupable_remove(self, item):
+        """Do not allow the text to be removed.
+        """
+        return 1
+
+    def on_groupable_iter(self):
+        """Return an iterator that can be used to traverse the children.
+        """
+        yield self.text
+        # alternative:
+        # return iter([self.text])
+
+    def on_groupable_length(self):
+        """Return the number of child objects, we have just the text object.
+        """
+        return 1
+
+    def on_groupable_pos(self, item):
+        """Return the position of the item wrt other child objects.
+        (we have only one child).
+        """
+        if item == self.text:
+            return 0
+        else:
+            return -1
+
+gobject.type_register(MyBox2)
+# add CanvasItem callbacks
+diacanvas.set_callbacks(MyBox2)
+# make the item groupable
+diacanvas.set_groupable(MyBox2)
+
+class MyBox3(diacanvas.CanvasElement, diacanvas.CanvasAbstractGroup):
+    def __init__(self):
+        self.__gobject_init__()
+
+        # create our line shapes (a red and a green line)
+        self.top_line = shape.Path()
+        self.top_line.set_color(diacanvas.color(255, 0, 0))
+        self.top_line.set_line_width(2)
+        self.bottom_line = shape.Path()
+        self.bottom_line.set_color(diacanvas.color(0, 255, 0))
+        self.bottom_line.set_line_width(2)
+        self.ellipse = shape.Ellipse()
+        self.ellipse.set_color(diacanvas.color(0, 0, 255))
+        self.ellipse.set_line_width(2)
+
+        self.h = diacanvas.Handle(self)
+        self.h.set_property('connectable',True)
+        self.h.set_property('movable',False)
+
+        
+        # create a text object (CanvasText is a composite object)
+        #self.text = diacanvas.CanvasText()
+        #self.text.set(text="hello")
+        # make the text a child of this canvas item
+        #self.text.set_child_of(self)
+        #self.add_construction(self.text)
+        
+    def on_update(self, affine):
+        # create a line on the top
+        # (line() takes one argument: a list of points)
+        self.top_line.line([(0, 0), (self.width, 0)])
+
+        # create a line on the bottom
+        self.bottom_line.line([(0, self.height), (self.width, self.height)])
+
+        # Draw the ellipse in the middle
+        self.ellipse.ellipse(center=(self.width/2,self.height/2), width=self.width/4, height=self.height/4)
+
+        # move the handle to the middle
+        self.h.set_pos_i(self.width/2,self.height/2)
+
+        # give the text the same width and height as out object
+        #self.text.set(width=self.width, height=self.height)
+        
+        # update the text
+        #self.update_child(self.text, affine)
+
+        # update the parent
+        diacanvas.CanvasElement.on_update(self, affine)
+
+        # expand the boundries of this canvas item so the lines (with
+        # width 2.0) are inside the boundries of this canvas item.
+        self.expand_bounds(1.0)
+
+    def on_shape_iter(self):
+        # an iterator 
+        yield self.top_line
+        yield self.bottom_line
+        yield self.ellipse
+        # alternative:
+        #return iter([self.top_line, self.bottom_line])
+
+    def on_event(self, event):
+        # make sure key events are send to our text object
+        if event.type == diacanvas.EVENT_KEY_PRESS:
+            print 'key'
+        #    self.text.focus()
+        #    return self.text.on_event(event)
+        else:
+            return diacanvas.CanvasElement.on_event(self, event)
+
+    # Groupable
+
+    def on_groupable_add(self, item):
+        """Add a new item. This is not allowed in this case.
+        """
+        return 0
+
+    def on_groupable_remove(self, item):
+        """Do not allow the text to be removed.
+        """
+        return 1
+
+    #def on_groupable_iter(self):
+    #    """Return an iterator that can be used to traverse the children.
+    #    """
+    #    #yield self.text
+    #    # alternative:
+    #    # return iter([self.text])
+
+    def on_groupable_length(self):
+        """Return the number of child objects, we have just the text object.
+        """
+        return 1
+
+    def on_groupable_pos(self, item):
+        """Return the position of the item wrt other child objects.
+        (we have only one child).
+        """
+        #if item == self.text:
+        #    return 0
+        #else:
+        #    return -1
+        return -1
+
+gobject.type_register(MyBox3)
+# add CanvasItem callbacks
+diacanvas.set_callbacks(MyBox3)
+# make the item groupable
+diacanvas.set_groupable(MyBox3)
--- src/conduit.py
+++ src/conduit.py
@@ -1,53 +1,8 @@
-import sys
 import gtk
-import gtk.glade
-import diacanvas
-import MyBox
-
-class conduitGui:
-    def __init__(self):
-        self.gladefile = "conduit.glade"
-        self.mainwindowname = "window1"
-        self.wTree = gtk.glade.XML(self.gladefile, self.mainwindowname)
-        self.win = self.wTree.get_widget(self.mainwindowname)
-        self.win.maximize()
-        dic = {"on_window1_destroy" : gtk.main_quit,
-            "on_synchronizebutton_clicked" : self.synchronizeSet,
-            "on_configurebutton_clicked" : self.configureItem,
-            "on_linkitemsbutton_clicked" : self.linkItem
-            }
-         
-        self.wTree.signal_autoconnect(dic)
-    
-        #insert the canvas
-        self.canvas = diacanvas.Canvas()
-        self.box = MyBox.MyBox() 
-        #box = diacanvas.CanvasBox()
-        #box.set(border_width=0.3, color=diacanvas.color(150, 150, 150, 128))
-        self.canvas.root.add(self.box)
-        self.box.move(100,100)
-
-        self.canvasW = self.wTree.get_widget("canvasScrolledWindow")
-        view = diacanvas.CanvasView(canvas = self.canvas)
-        view.show()
-        self.canvasW.add(view)
-        return
-     
-    # callbacks.
-    def synchronizeSet(self, widget):
-    	print "clicked synchronize"
-    
-
-    def configureItem(self, widget):
-    	print "clicked configure"
-
-
-    def linkItem(self, widget):
-    	print "clicked link"
-
+import conduit.MainWindow
 
 if __name__ == "__main__":
-    app = conduitGui()
+    app = MainWindow()
     gtk.main()
      
  
