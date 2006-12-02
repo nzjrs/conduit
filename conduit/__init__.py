7a80b4e68effeafeda2b2b41e1fb5a091271d7e2
John Stowers <jstowers@src.gnome.org>
2006-12-02 10:05:50 +0000

    Add some memory analysis functions
    svn path=/trunk/; revision=198

--- conduit/__init__.py
+++ conduit/__init__.py
@@ -24,6 +24,9 @@ License: GPLv2
 import logging
 import os
 import gobject
+gobject.threads_init()
+
+import Settings
 
 APPNAME="Conduit"
 APPVERSION="0.3.0"
@@ -59,12 +62,65 @@ except KeyError:
 
 print "Log Level =", LOG_LEVEL
 logging.basicConfig(level=level,
-                    format='[%(levelname)s] %(message)s (%(filename)s)')
+                    format='[%(levelname)s] %(message)s')
 #                    filename='/tmp/myapp.log',
 #                    filemode='w')
 
-import Settings
 
-gobject.threads_init()
+
+#Shorthand log functions to save typing
+def log(message):
+    logging.info(message)
+def logd(message):
+    logging.debug(message)
+def logw(message):
+    logging.warn(message)
+
+#Memory analysis functions
+#Taken from
+#http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/286222
+_proc_status = '/proc/%d/status' % os.getpid()
+_scale = {'kB': 1024.0, 'mB': 1024.0*1024.0,
+          'KB': 1024.0, 'MB': 1024.0*1024.0}
+
+def _VmB(VmKey):
+    global _proc_status, _scale
+     # get pseudo file  /proc/<pid>/status
+    try:
+        t = open(_proc_status)
+        v = t.read()
+        t.close()
+    except:
+        return 0.0  # non-Linux?
+     # get VmKey line e.g. 'VmRSS:  9999  kB\n ...'
+    i = v.index(VmKey)
+    v = v[i:].split(None, 3)  # whitespace
+    if len(v) < 3:
+        return 0.0  # invalid format?
+     # convert Vm value to bytes
+    return float(v[1]) * _scale[v[2]]
+
+def memory(since=0.0):
+    """
+    Return memory usage in bytes.
+    """
+    return _VmB('VmSize:') - since
+
+
+def resident(since=0.0):
+    """
+    Return resident memory usage in bytes.
+    """
+    return _VmB('VmRSS:') - since
+
+
+def stacksize(since=0.0):
+    """    
+    Return stack size in bytes.
+    """
+    return _VmB('VmStk:') - since
+
+
+#Globale settings object to be used by all
 settings = Settings.Settings()
 
