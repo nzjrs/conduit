c6d033e8c52602935c6c656238895fc9f6fef5e6
John Carr <johncarr@src.gnome.org>
2006-12-24 22:27:31 +0000

    Improve SynCE (WM5) support - can now fetch xml-contact-string data into individual objects
    svn path=/trunk/; revision=227

--- conduit/dataproviders/SynceModule.py
+++ conduit/dataproviders/SynceModule.py
@@ -16,6 +16,7 @@ import traceback
 import dbus
 import dbus.glib
 
+import threading
 
 # bit of a hack, will fix itself when SYNCE can be a dynamic provider..
 phone_category = conduit.DataProvider.DataProviderCategory(
@@ -24,62 +25,80 @@ phone_category = conduit.DataProvider.DataProviderCategory(
                         "Phone")
 
 MODULES = {
-	"SynceContactTwoWay" : {
-		"name": _("Contacts"),
-		"description": _("Source for synchronizing Windows Mobile Phones"),
-		"type": "source",
-		"category": phone_category,
-		"in_type": "text",
-		"out_type": "text",
+        "SynceContactTwoWay" : {
+                "name": _("Contacts"),
+                "description": _("Source for synchronizing Windows Mobile Phones"),
+                "type": "source",
+                "category": phone_category,
+                "in_type": "text",
+                "out_type": "text",
                 "icon": "tomboy"
-	},
+        },
+        "SynceCalendarTwoWay" : {
+                "name": _("Calendar"),
+                "description": _("Source for synchronizing Windows Mobile Phones"),
+                "type": "source",
+                "category": phone_category,
+                "in_type": "text",
+                "out_type": "text",
+                "icon": "tomboy"
+        },
 }
 
-class SynceContactTwoWay(DataProvider.TwoWay):
-    def __init__(self, *args):
-        DataProvider.TwoWay.__init__(self, _("Contacts"), _("Source for synchronizing Windows Mobile Phones"), "tomboy")
-        self.contacts = None
-        self.name_to_id = None
+class SynceTwoWay(DataProvider.TwoWay):
+    def __init__(self, obj_type, *args):
+        self.obj_type = obj_type
+        self.objects = None
 
     def refresh(self):
         DataProvider.TwoWay.refresh(self)
         
-        self.contacts = []
+        self.objects = []
         
         bus = dbus.SessionBus()
         proxy_obj = bus.get_object("org.synce.SyncEngine", "/org/synce/SyncEngine")
         self.synce = dbus.Interface(proxy_obj, "org.synce.SyncEngine")
 
-        self.name_to_id = {}
+        name_to_id = {}
         for id, name in self.synce.GetItemTypes().items():
-            self.name_to_id[name.lower()] = id
-        
+            name_to_id[name.lower()] = id
+        self.obj_type_id = name_to_id[self.obj_type]
+
         # temporary workarounds
         self._temp_delete_partnership()
         self._temp_create_partnership()
 
         self.synce.StartSync()
-       
+        self.synce.connect_to_signal("Synchronized", self._synchronized_cb)
+
+        self.synchronized_ev = threading.Event()
+        self.synchronized_ev.wait(10)
+
         items = []
-        items.append(self.name_to_id["contacts"])
+        items.append(name_to_id["contacts"])
 
         chgs = self.synce.GetRemoteChanges(items)
-        logging.info(chgs)
-        self.contacts.append(str(chgs))
-        
+        for chg_type in chgs:
+            for luid,changetype,obj in chgs[chg_type]:
+                self.objects.append(str(obj))
+
+    def _synchronized_cb(self):
+        logging.info("event fired, my sweet children...")
+        self.synchronized_ev.set()
+
     def get(self, index):
         DataProvider.TwoWay.get(self, index)
-        return self.contacts[index]
+        return self.objects[index]
                 
     def get_num_items(self):
         DataProvider.TwoWay.get_num_items(self)
-        return len(self.contacts)
+        return len(self.objects)
 
     def put(self, contact, putContactOnTopOf):
         DataProvider.TwoWay.put(contact, putContactOnTopOf)
 
     def _temp_create_partnership(self):
-        self.synce.CreatePartnership(".conduit", (self.name_to_id["contacts"],))
+        self.synce.CreatePartnership(".conduit", (self.obj_type_id,))
 
     def _temp_delete_partnership(self):
         #FIXME: this is surely to got a dirty hack!
@@ -89,5 +108,16 @@ class SynceContactTwoWay(DataProvider.TwoWay):
                 self.synce.DeletePartnership(id)
 
     def finish(self):
+        # self.synce.AckChanges
         self._temp_delete_partnership()
         self.contacts = None
+
+class SynceContactTwoWay(SynceTwoWay):
+    def __init__(self, *args):
+        DataProvider.TwoWay.__init__(self, _("Contacts"), _("Sync your Windows Mobile Phones"), "tomboy")
+        SynceTwoWay.__init__(self,"contacts",args)
+
+class SynceCalendarTwoWay(SynceTwoWay):
+    def __init__(self, *args):
+        DataProvider.TwoWay.__init__(self, _("Calendar"), _("Sync your Windows Mobile Phones"), "tomboy")
+        SynceTwoWay.__init__(self,"calendar",args)
