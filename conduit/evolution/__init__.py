9e5320ddadbc9b8326e1ac65463df79ec83e5417
John Stowers <jstowers@src.gnome.org>
2006-12-30 10:31:14 +0000

    Delightful autotools hack to support uninstalled conduit with evolution support
    svn path=/trunk/; revision=282

--- conduit/evolution/__init__.py
+++ conduit/evolution/__init__.py
@@ -1,1 +1,13 @@
-from _evolution import *
+try:
+    from _evolution import *
+except ImportError:
+    #Autotools hack to import the lib from the hidden .libs directory
+    import os, sys
+    currentdir = os.path.abspath(os.path.dirname(__file__))
+    libdir = os.path.join(currentdir,".libs")
+    if os.path.isfile(os.path.join(libdir,"_evolution.so")):
+        sys.path.insert(0, libdir)
+        from _evolution import *
+    else:
+        print "PLEASE BUILD CONDUIT TO GET EVOLUTION SUPPORT"
+        raise ImportError
