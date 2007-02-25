f5068c2721ab38335a3a598a4f823ab4779f7a1d
John Stowers <jstowers@src.gnome.org>
2007-02-25 10:12:50 +0000

    add new test
    svn path=/trunk/; revision=379

---
+++ test/python-tests/TestConvert.py
@@ -0,0 +1,54 @@
+#common sets up the conduit environment
+from common import *
+
+from conduit.Module import ModuleManager
+from conduit.TypeConverter import TypeConverter
+
+
+#Dynamically load all datasources, datasinks and converters
+dirs_to_search =    [
+                    os.path.join(conduit.SHARED_MODULE_DIR,"dataproviders"),
+                    os.path.join(conduit.USER_DIR, "modules")
+                    ]
+model = ModuleManager(dirs_to_search)
+type_converter = TypeConverter(model)
+
+TYPES = {
+    "file"      :   conduit.datatypes.File.File,
+    "note"      :   conduit.datatypes.Note.Note,
+    "event"     :   conduit.datatypes.Event.Event,
+    "contact"   :   conduit.datatypes.Contact.Contact
+    }
+
+WIDTH=9
+def pad(s):
+    return s+" "*(WIDTH-len(s))
+
+def header(items):
+    return " "*WIDTH+"|"+"|".join([pad(i) for i in items])+"|"
+
+def row(entries, header):
+    s = ""
+    for i in entries:
+        s += "%s|" % pad(i)
+    s += "\n"
+    for i in entries:
+        s += "%s|" % ("-"*WIDTH)
+    return s
+
+#Draw a table of the available conversions
+print header(TYPES.keys())
+for i in TYPES:
+    conversions = [i]
+    for j in TYPES:
+        if i == j:
+            conversions.append("N/A")
+        else:
+            if type_converter.conversion_exists(i,j,False):
+                conversions.append("Y")
+            elif type_converter.conversion_exists(i,j,True):
+                conversions.append("Y*")
+            else:
+                conversions.append("N")
+    print row(conversions,TYPES.keys())
+
