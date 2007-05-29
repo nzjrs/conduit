773fbf8a2d0bc9bc11dfd588b41e29427804d6c9
John Carr <johncarr@src.gnome.org>
2007-05-29 21:22:52 +0000

    First pass at making EvoContact a twoway affair
    svn path=/trunk/; revision=576

--- conduit/dataproviders/EvolutionModule/EvolutionModule.py
+++ conduit/dataproviders/EvolutionModule/EvolutionModule.py
@@ -2,7 +2,7 @@ try:
     import evolution as evo
 
     MODULES = {
-    	"EvoContactSource" : { "type": "dataprovider" }	
+    	"EvoContactTwoWay" : { "type": "dataprovider" }	
     }
 except ImportError:
     MODULES = {}
@@ -15,15 +15,18 @@ from conduit import log,logd,logw
 import conduit.DataProvider as DataProvider
 import conduit.datatypes.Contact as Contact
 import conduit.Utils as Utils
+import conduit.Exceptions as Exceptions
 
-class EvoContactSource(DataProvider.DataSource):
+import datetime
+
+class EvoContactTwoWay(DataProvider.DataSource):
 
     DEFAULT_ADDRESSBOOK = "default"
 
     _name_ = "Evolution Contacts"
     _description_ = "Sync your Contacts"
     _category_ = DataProvider.CATEGORY_LOCAL
-    _module_type_ = "source"
+    _module_type_ = "twoway"
     _in_type_ = "contact"
     _out_type_ = "contact"
     _icon_ = "contact-new"
@@ -32,7 +35,7 @@ class EvoContactSource(DataProvider.DataSource):
         DataProvider.DataSource.__init__(self)
         self.contacts = None
 
-        self.selectedAddressBook = EvoContactSource.DEFAULT_ADDRESSBOOK
+        self.selectedAddressBook = EvoContactTwoWay.DEFAULT_ADDRESSBOOK
 
         self._addressBooks = evo.list_addressbooks()
 
@@ -40,8 +43,8 @@ class EvoContactSource(DataProvider.DataSource):
         tree = Utils.dataprovider_glade_get_widget(
                         __file__, 
                         "config.glade",
-						"ContactConfigDialog"
-						)
+                        "ContactConfigDialog"
+                        )
         
         #get a whole bunch of widgets
         bookComboBox = tree.get_widget("bookComboBox")
@@ -58,25 +61,6 @@ class EvoContactSource(DataProvider.DataSource):
             rowref = store.append( (name, id) )
             if id == self.selectedAddressBook:
                 bookComboBox.set_active_iter(rowref)
-
-
-
-#        cb = gtk.combo_box_new_text()
-#        for displayname,  in self._addressBooks:
-#            cb.append_text(b)
-
-#        vbox.pack_end(c)
-#        cb.show()
-        
-        #preload the widgets
-        #if self.limit > 0:
-        #    limitCb.set_active(True)
-        #    limitSb.set_value(self.limit)
-        #else:
-        #    limitCb.set_active(False)
-        #url.set_text(self.feedUrl)
-        #photosCb.set_active(self.downloadPhotos)
-        #audioCb.set_active(self.downloadAudio)
         
         dlg = tree.get_widget("ContactConfigDialog")
         dlg.set_transient_for(window)
@@ -91,13 +75,9 @@ class EvoContactSource(DataProvider.DataSource):
         DataProvider.DataSource.refresh(self)
         self.contacts = []
 
-        book = evo.open_addressbook('default')
-        for i in book.get_all_contacts():
-            contact = Contact.Contact(None)
-            contact.set_from_vcard_string(i.get_vcard_string())
-            contact.set_UID(i.get_uid())
-
-            self.contacts.append(contact)
+        self.book = evo.open_addressbook(self.selectedAddressBook)
+        for i in self.book.get_all_contacts():
+            self.contacts.append(i.get_uid())
 
     def get_num_items(self):
         DataProvider.DataSource.get_num_items(self)
@@ -105,7 +85,46 @@ class EvoContactSource(DataProvider.DataSource):
 
     def get(self, index):
         DataProvider.DataSource.get(self, index)
-        return self.contacts[index]
+        uid = self.contacts[index]
+        return self._get(uid)
+
+    def _get(self, LUID):
+        """
+        Retrieve a specific contact object from evolution
+        FIXME: In 0.5 this will replace get(...)
+        """
+        obj = self.book.get_contact(LUID)
+        contact = Contact.Contact(None)
+        contact.set_from_vcard_string(obj.get_vcard_string())
+        contact.set_UID(obj.get_uid())
+        contact.set_mtime(datetime.datetime.fromtimestamp(obj.get_modified()))
+        return contact
+
+    def put(self, contact, overwrite, LUID=None):
+        if LUID != None:
+            obj = self.book.get_contact(LUID)
+            if obj != None:
+                if overwrite == True:
+                    # overwrite
+                    # FIXME: Overwrite + return LUID instead of falling back to creating a new object
+                    self.book.remove_contact_by_id(LUID)
+                else:
+                    existingContact = self._get(LUID)
+                    comp = contact.compare(existingContact)
+                    # only update if newer
+                    if comp != conduit.datatypes.COMPARISON_NEWER:
+                        raise Exceptions.SynchronizeConflictError(comp, contact, existingContact)
+                    else:
+                        # FIXME: Overwrite + return LUID instead of falling back to creating a new object
+                        self.book.remove_contact_by_id(LUID)
+
+        # if we get here then it is new...
+        obj = evo.EContact(vcard=contact.get_vcard_string(), uid=LUID)
+        self.book.add_contact(obj)
+        return obj.get_uid()
+
+    def delete(self, LUID):
+        self.book.remove_contact_by_id(LUID)
 
     def finish(self):
         DataProvider.DataSource.finish(self)
@@ -117,7 +136,7 @@ class EvoContactSource(DataProvider.DataSource):
             }
 
     def set_configuration(self, config):
-        self.selectedAddressBook = config.get("selectedAddressBook", EvoContactSource.DEFAULT_ADDRESSBOOK)
+        self.selectedAddressBook = config.get("selectedAddressBook", EvoContactTwoWay.DEFAULT_ADDRESSBOOK)
 
     def get_UID(self):
         #return the uri of the evo addressbook in use
