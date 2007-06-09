e7d213ec5dc04e8807a17b3406dc753bf49ccbda
John Carr <johncarr@src.gnome.org>
2007-06-09 21:53:02 +0000

    Allow contact datatype to be pickled
    svn path=/trunk/; revision=631

--- conduit/datatypes/Contact.py
+++ conduit/datatypes/Contact.py
@@ -23,6 +23,26 @@ class Contact(DataType.DataType):
     def compare(self, B):
         return conduit.datatypes.COMPARISON_UNKNOWN
 
+    def __getstate__(self):
+        """
+        Store the object state in a dict for pickling
+        """
+        data = {}
+        data['vcard'] = self.get_vcard_string()
+        data['mtime'] = self.get_mtime()
+        data['uid'] = self.get_UID()
+        data['open_uri'] = self.get_open_URI()
+        return data
+
+    def __setstate__(self, data):
+        """
+        Set object state from dict (after unpickling)
+        """
+        self.set_from_vcard_string(data['vcard'])
+        self.set_mtime(data['mtime'])
+        self.set_UID(data['uid'])
+        self.set_open_URI(data['open_uri'])
+
     def __str__(self):
         return self.get_vcard_string()
 
