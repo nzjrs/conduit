16804de70e1ab5ec78e00072397c3cc8e37a33b1
John Carr <johncarr@src.gnome.org>
2007-06-17 14:47:56 +0000

    Evo Calendar + Tasks sync. Also, reuse more code. C+P is bad.
    svn path=/trunk/; revision=645

--- conduit/dataproviders/EvolutionModule/EvolutionModule.py
+++ conduit/dataproviders/EvolutionModule/EvolutionModule.py
@@ -3,9 +3,10 @@ try:
     import evolution as evo
     if evo.__version__ == (0,0,1):
         MODULES = {
-        	"EvoContactTwoWay" : { "type": "dataprovider" },
-        	"EvoCalendarTwoWay" : { "type": "dataprovider" },
-        	"EvoMemoTwoWay" : { "type": "dataprovider" },
+                "EvoContactTwoWay"  : { "type": "dataprovider" },
+                "EvoCalendarTwoWay" : { "type": "dataprovider" },
+                "EvoTasksTwoWay"    : { "type": "dataprovider" },
+                "EvoMemoTwoWay"     : { "type": "dataprovider" },
         }
 except:
     pass
@@ -26,9 +27,69 @@ import conduit.datatypes.Note as Note
 
 import datetime
 
-class EvoBase:
-    def __init__(self):
-        pass
+class EvoBase(DataProvider.TwoWay):
+    def __init__(self, *args):
+        DataProvider.TwoWay.__init__(self)
+        self.uids = None
+
+    def _get_object(self, uid):
+        raise NotImplementedError
+
+    def _create_object(self, obj):
+        raise NotImplementedError
+
+    def _update_object(self, uid, obj):
+        if self._delete_object(obj):
+            uid = self._create_object(obj)
+            return uid
+        else:
+            raise Exceptions.SyncronizeError("Error updating object (uid: %s)" % uid)
+
+    def _delete_object(self, uid):
+        raise NotImplementedError
+
+    def refresh(self):
+        DataProvider.TwoWay.refresh(self)
+        self.uids = []
+
+    def get_all(self):
+        DataProvider.TwoWay.get_all(self)
+        return self.uids
+
+    def get(self, LUID):
+        DataProvider.TwoWay.get(self, LUID)
+        return self._get_object(LUID)
+
+    def put(self, obj, overwrite, LUID=None):
+        DataProvider.TwoWay.put(self, obj, overwrite, LUID)
+        if LUID != None:
+            existing = self._get_object(LUID)
+            if existing != None:
+                if overwrite == True:
+                    uid = self._update_object(LUID, obj)
+                    return uid
+                else:
+                    comp = obj.compare(existing)
+                    # only update if newer
+                    if comp != conduit.datatypes.COMPARISON_NEWER:
+                        raise Exceptions.SynchronizeConflictError(comp, existing, obj)
+                    else:
+                        # overwrite and return new ID
+                        uid = self._update_object(LUID, obj)
+                        return uid
+
+        # if we get here then it is new...
+        log("Creating new object")
+        uid = self._create_object(obj)
+        return uid
+
+    def delete(self, LUID):
+        if not self._delete_object(LUID):
+            logw("Error deleting event (uid: %s)" % LUID)
+
+    def finish(self):
+        DataProvider.TwoWay.finish(self)
+        self.uids = None
 
     def configure(self, window, selected, sources, name):
         tree = Utils.dataprovider_glade_get_widget(
@@ -66,7 +127,7 @@ class EvoBase:
         dlg.destroy()  
         return selected
 
-class EvoContactTwoWay(DataProvider.TwoWay, EvoBase):
+class EvoContactTwoWay(EvoBase):
 
     DEFAULT_ADDRESSBOOK_URI = "default"
 
@@ -79,14 +140,11 @@ class EvoContactTwoWay(DataProvider.TwoWay, EvoBase):
     _icon_ = "contact-new"
 
     def __init__(self, *args):
-        DataProvider.TwoWay.__init__(self)
         EvoBase.__init__(self)
-        self.contacts = None
-
         self.addressBookURI = EvoContactTwoWay.DEFAULT_ADDRESSBOOK_URI
         self._addressBooks = evo.list_addressbooks()
 
-    def _get_contact(self, LUID):
+    def _get_object(self, LUID):
         """
         Retrieve a specific contact object from evolution
         FIXME: In 0.5 this will replace get(...)
@@ -98,22 +156,22 @@ class EvoContactTwoWay(DataProvider.TwoWay, EvoBase):
         contact.set_mtime(datetime.datetime.fromtimestamp(obj.get_modified()))
         return contact
 
-    def _create_contact(self, contact):
+    def _create_object(self, contact):
         obj = evo.EContact(vcard=contact.get_vcard_string())
         if self.book.add_contact(obj):
             return obj.get_uid()
         else:
             raise Exceptions.SyncronizeError("Error creating contact")
 
-    def _delete_contact(self, uid):
+    def _delete_object(self, uid):
         return self.book.remove_contact_by_id(uid)
 
-    def _update_contact(self, uid, contact):
-        if self._delete_contact(uid):
-            uid = self._create_contact(contact)
-            return uid
-        else:
-            raise Exceptions.SyncronizeError("Error updating contact (uid: %s)" % uid)
+    def refresh(self):
+        EvoBase.refresh(self)
+        
+        self.book = evo.open_addressbook(self.addressBookURI)
+        for i in self.book.get_all_contacts():
+            self.uids.append(i.get_uid())
 
     def configure(self, window):
         self.addressBookURI = EvoBase.configure(self, 
@@ -122,54 +180,6 @@ class EvoContactTwoWay(DataProvider.TwoWay, EvoBase):
                                     self._addressBooks,
                                     "Addressbook"
                                     )
-
-    def refresh(self):
-        DataProvider.TwoWay.refresh(self)
-        self.contacts = []
-
-        self.book = evo.open_addressbook(self.addressBookURI)
-        for i in self.book.get_all_contacts():
-            self.contacts.append(i.get_uid())
-
-    def get_all(self):
-        DataProvider.TwoWay.get_all(self)
-        return self.contacts
-
-    def get(self, LUID):
-        DataProvider.TwoWay.get(self, LUID)
-        return self._get_contact(LUID)
-
-    def put(self, contact, overwrite, LUID=None):
-        if LUID != None:
-            obj = self.book.get_contact(LUID)
-            if obj != None:
-                if overwrite == True:
-                    # overwrite and return new ID
-                    uid = self._update_contact(LUID, contact)
-                    return uid
-                else:
-                    existingContact = self._get_contact(LUID)
-                    comp = contact.compare(existingContact)
-                    # only update if newer
-                    if comp != conduit.datatypes.COMPARISON_NEWER:
-                        raise Exceptions.SynchronizeConflictError(comp, existingContact, contact)
-                    else:
-                        # overwrite and return new ID
-                        uid = self._update_contact(LUID, contact)
-                        return uid
-
-        # if we get here then it is new...
-        log("Creating new contact")
-        uid = self._create_contact(contact)
-        return uid
-
-    def delete(self, LUID):
-        if not self._delete_contact(LUID):
-            logw("Error deleting contact (uid: %s)" % LUID)
-
-    def finish(self):
-        DataProvider.TwoWay.finish(self)
-        self.contacts = None
 
     def get_configuration(self):
         return {
@@ -183,7 +193,7 @@ class EvoContactTwoWay(DataProvider.TwoWay, EvoBase):
         #return the uri of the evo addressbook in use
         return self.addressBookURI
 
-class EvoCalendarTwoWay(DataProvider.TwoWay, EvoBase):
+class EvoCalendarTwoWay(EvoBase):
 
     DEFAULT_CALENDAR_URI = "default"
 
@@ -196,14 +206,11 @@ class EvoCalendarTwoWay(DataProvider.TwoWay, EvoBase):
     _icon_ = "contact-new"
 
     def __init__(self, *args):
-        DataProvider.TwoWay.__init__(self)
         EvoBase.__init__(self)
-        self.events = None
-
         self.calendarURI = EvoCalendarTwoWay.DEFAULT_CALENDAR_URI
-        self._calendarURIs = evo.list_addressbooks()
+        self._calendarURIs = evo.list_calendars()
 
-    def _get_event(self, LUID):
+    def _get_object(self, LUID):
         """
         Get an event from Evolution.
         """
@@ -214,22 +221,26 @@ class EvoCalendarTwoWay(DataProvider.TwoWay, EvoBase):
         event.set_mtime(datetime.datetime.fromtimestamp(raw.get_modified()))
         return event
 
-    def _create_event(self, event):
+    def _create_object(self, event):
+        # work around.. (avoid duplicate UIDs)
+        if "UID" in [x.name for x in list(event.iCal.lines())]:
+            event.iCal.remove(event.iCal.uid)
+
         obj = evo.ECalComponent(evo.CAL_COMPONENT_EVENT, event.get_ical_string())
         if self.calendar.add_object(obj):
             return obj.get_uid()
         else:
             raise Exceptions.SyncronizeError("Error creating event")
 
-    def _update_event(self, uid, event):
-        if self._delete_event(uid):
-            uid = self._create_event(event)
-            return uid
-        else:
-            raise Exceptions.SyncronizeError("Error updating calendar (uid: %s)" % uid)
-
-    def _delete_event(self, uid):
+    def _delete_object(self, uid):
         return self.calendar.remove_object(self.calendar.get_object(uid, ""))
+
+    def refresh(self):
+        EvoBase.refresh(self)
+        
+        self.calendar = evo.open_calendar_source(self.calendarURI, evo.CAL_SOURCE_TYPE_EVENT)
+        for i in self.calendar.get_all_objects():
+            self.uids.append(i.get_uid())
 
     def configure(self, window):
         self.calendarURI = EvoBase.configure(self, 
@@ -238,52 +249,6 @@ class EvoCalendarTwoWay(DataProvider.TwoWay, EvoBase):
                                     self._calendarURIs,
                                     "Calendar"
                                     )
-
-    def refresh(self):
-        DataProvider.TwoWay.refresh(self)
-        self.events = []
-
-        self.calendar = evo.open_calendar_source(self.calendarURI, 0)
-        for i in self.calendar.get_all_objects():
-            self.events.append(i.get_uid())
-
-    def get_all(self):
-        DataProvider.TwoWay.get_all(self)
-        return self.events
-
-    def get(self, LUID):
-        DataProvider.TwoWay.get(self, LUID)
-        return self._get_event(LUID)
-
-    def put(self, event, overwrite, LUID=None):
-        if LUID != None:
-            existing = self._get_event(LUID)
-            if existing != None:
-                if overwrite == True:
-                    uid = self._update_event(LUID, event)
-                    return uid
-                else:
-                    comp = event.compare(existing)
-                    # only update if newer
-                    if comp != conduit.datatypes.COMPARISON_NEWER:
-                        raise Exceptions.SynchronizeConflictError(comp, existing, event)
-                    else:
-                        # overwrite and return new ID
-                        uid = self._update_event(LUID, event)
-                        return uid
-
-        # if we get here then it is new...
-        log("Creating new event")
-        uid = self._create_event(event)
-        return uid
-
-    def delete(self, LUID):
-        if not self._delete_event(LUID):
-            logw("Error deleting event (uid: %s)" % LUID)
-
-    def finish(self):
-        DataProvider.TwoWay.finish(self)
-        self.events = None
 
     def get_configuration(self):
         return {
@@ -297,7 +262,72 @@ class EvoCalendarTwoWay(DataProvider.TwoWay, EvoBase):
         #return the uri of the evo calendar in use
         return self.calendarURI
 
-class EvoMemoTwoWay(DataProvider.TwoWay, EvoBase):
+class EvoTasksTwoWay(EvoBase):
+
+    DEFAULT_URI = "default"
+
+    _name_ = "Evolution Tasks"
+    _description_ = "Sync your Tasks"
+    _category_ = DataProvider.CATEGORY_OFFICE
+    _module_type_ = "twoway"
+    _in_type_ = "event"
+    _out_type_ = "event"
+    _icon_ = "tomboy"
+
+    def __init__(self, *args):
+        EvoBase.__init__(self)
+        self.uri = self.DEFAULT_URI
+        self._uris = evo.list_task_sources()
+
+    def _get_object(self, LUID):
+        raw = self.tasks.get_object(LUID, "")
+        task = Event.Event(None)
+        task.set_from_ical_string(raw.get_as_string())
+        task.set_UID(raw.get_uid())
+        task.set_mtime(datetime.datetime.fromtimestamp(raw.get_modified()))
+        return task
+
+    def _create_object(self, event):
+        # work around.. (avoid duplicate UIDs)
+        if "UID" in [x.name for x in list(event.iCal.lines())]:
+            event.iCal.remove(event.iCal.uid)
+
+        obj = evo.ECalComponent(evo.CAL_COMPONENT_TODO, event.get_ical_string())
+        if self.tasks.add_object(obj):
+            return obj.get_uid()
+        else:
+            raise Exceptions.SyncronizeError("Error creating event")
+
+    def _delete_object(self, uid):
+        return self.tasks.remove_object(self.tasks.get_object(uid, ""))
+
+    def refresh(self):
+        EvoBase.refresh(self)
+        self.tasks = evo.open_calendar_source(self.uri, evo.CAL_SOURCE_TYPE_TODO)
+        for i in self.tasks.get_all_objects():
+            self.uids.append(i.get_uid())
+
+    def configure(self, window):
+        self.uri = EvoBase.configure(self, 
+                                    window, 
+                                    self.uri, 
+                                    self._uris,
+                                    "Tasks"
+                                    )
+
+    def get_configuration(self):
+        return {
+            "tasksURI" : self.uri
+            }
+
+    def set_configuration(self, config):
+        self.uri = config.get("tasksURI", self.DEFAULT_URI)
+
+    def get_UID(self):
+        #return the uri of the evo calendar in use
+        return self.uri
+
+class EvoMemoTwoWay(EvoBase):
 
     DEFAULT_ADDRESSBOOK_URI = "default"
 
@@ -310,14 +340,13 @@ class EvoMemoTwoWay(DataProvider.TwoWay, EvoBase):
     _icon_ = "tomboy"
 
     def __init__(self, *args):
-        DataProvider.TwoWay.__init__(self)
         EvoBase.__init__(self)
         self.source = None
 
         self.memoSourceURI = ""
         self._memoSources = evo.list_memo_sources()
 
-    def _get_memo(self, LUID):
+    def _get_object(self, LUID):
         """
         Retrieve a specific contact object from evolution
         FIXME: In 0.5 this will replace get(...)
@@ -335,7 +364,7 @@ class EvoMemoTwoWay(DataProvider.TwoWay, EvoBase):
         print note.get_note_string()
         return note
 
-    def _create_memo(self, note):
+    def _create_object(self, note):
         obj = evo.ECalComponent(evo.CAL_COMPONENT_JOURNAL)
         obj.set_summary(note.title)
         obj.set_description(note.contents)
@@ -346,15 +375,14 @@ class EvoMemoTwoWay(DataProvider.TwoWay, EvoBase):
         else:
             raise Exceptions.SyncronizeError("Error creating memo")
 
-    def _delete_memo(self, uid):
+    def _delete_object(self, uid):
         return self.source.remove_object(uid)
 
-    def _update_memo(self, uid, memo):
-        if self._delete_memo(uid):
-            uid = self._create_memo(memo)
-            return uid
-        else:
-            raise Exceptions.SyncronizeError("Error updating memo (uid: %s)" % uid)
+    def refresh(self):
+        EvoBase.refresh(self)
+        self.source = evo.open_calendar_source(self.memoSourceURI, evo.CAL_SOURCE_TYPE_JOURNAL)
+        for i in self.source.get_all_objects():
+            self.uids.append(i.get_uid())
 
     def configure(self, window):
         self.memoSourceURI = EvoBase.configure(self, 
@@ -363,54 +391,6 @@ class EvoMemoTwoWay(DataProvider.TwoWay, EvoBase):
                                     self._memoSources,
                                     "Memo Source"
                                     )
-
-    def refresh(self):
-        DataProvider.TwoWay.refresh(self)
-        self.memos = []
-
-        self.source = evo.open_calendar_source(self.memoSourceURI, evo.CAL_SOURCE_TYPE_JOURNAL)
-        for i in self.source.get_all_objects():
-            self.memos.append(i.get_uid())
-
-    def get_all(self):
-        DataProvider.TwoWay.get_all(self)
-        return self.memos
-
-    def get(self, LUID):
-        DataProvider.TwoWay.get(self, LUID)
-        return self._get_memo(LUID)
-
-    def put(self, memo, overwrite, LUID=None):
-        if LUID != None:
-            obj = self.source.get_object(LUID, "")
-            if obj != None:
-                if overwrite == True:
-                    # overwrite and return new ID
-                    uid = self._update_memo(LUID, memo)
-                    return uid
-                else:
-                    existingMemo= self._get_memo(LUID)
-                    comp = contact.compare(existingMemo)
-                    # only update if newer
-                    if comp != conduit.datatypes.COMPARISON_NEWER:
-                        raise Exceptions.SynchronizeConflictError(comp, memo, existingMemo)
-                    else:
-                        # overwrite and return new ID
-                        uid = self._update_memo(LUID, memo)
-                        return uid
-
-        # if we get here then it is new...
-        log("Creating new memo")
-        uid = self._create_memo(memo)
-        return uid
-
-    def delete(self, LUID):
-        if not self._delete_memo(LUID):
-            logw("Error deleting memo (uid: %s)" % LUID)
-
-    def finish(self):
-        DataProvider.TwoWay.finish(self)
-        self.memos = None
 
     def get_configuration(self):
         return {
