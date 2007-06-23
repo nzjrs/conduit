1677955f29182e18f8a62403985b2bbb021a884a
John Stowers <jstowers@src.gnome.org>
2007-06-23 17:53:45 +0000

    Add support for box.net. Credit to thomas on this one
    svn path=/trunk/; revision=654

---
+++ conduit/dataproviders/BoxDotNetModule/BoxDotNetAPI/boxdotnet.py
@@ -0,0 +1,272 @@
+"""
+Python bindings for the Box.net API
+
+Copyright (c) 2007 Thomas Van Machelen <thomas dot vanmachelen at gmail dot com>
+Copyright (c) 2007 John Stowers <john dot stowers at gmail dot com>
+
+Upload, handler and XMLNode code adapted from flickrapi:
+Copyright (c) 2007 Brian "Beej Jorgensen" Hall
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import urllib
+import urllib2
+import mimetools
+import mimetypes
+import os
+import sys
+
+from xml.dom.minidom import parseString
+import xml.dom
+
+def get_content_type(filename):
+	return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+########################################################################
+# XML functionality
+########################################################################
+
+#-----------------------------------------------------------------------
+class XMLNode:
+    """XMLNode -- generic class for holding an XML node
+
+    xmlStr = \"\"\"<xml foo="32">
+    <name bar="10">Name0</name>
+    <name bar="11" baz="12">Name1</name>
+    </xml>\"\"\"
+
+    f = XMLNode.parseXML(xmlStr)
+
+    print f.elementName              # xml
+    print f['foo']                   # 32
+    print f.name                     # [<name XMLNode>, <name XMLNode>]
+    print f.name[0].elementName      # name
+    print f.name[0]["bar"]           # 10
+    print f.name[0].elementText      # Name0
+    print f.name[1].elementName      # name
+    print f.name[1]["bar"]           # 11
+    print f.name[1]["baz"]           # 12
+
+    """
+
+    def __init__(self):
+        """Construct an empty XML node."""
+        self.elementName=""
+        self.elementText=""
+        self.attrib={}
+        self.xml=""
+
+    def __setitem__(self, key, item):
+        """Store a node's attribute in the attrib hash."""
+        self.attrib[key] = item
+
+    def __getitem__(self, key):
+        """Retrieve a node's attribute from the attrib hash."""
+        return self.attrib[key]
+
+    #-----------------------------------------------------------------------
+    @classmethod
+    def parseXML(cls, xmlStr, storeXML=False):
+        """Convert an XML string into a nice instance tree of XMLNodes.
+
+        xmlStr -- the XML to parse
+        storeXML -- if True, stores the XML string in the root XMLNode.xml
+        """
+
+        def __parseXMLElement(element, thisNode):
+            """Recursive call to process this XMLNode."""
+            thisNode.elementName = element.nodeName
+
+            #print element.nodeName
+
+            # add element attributes as attributes to this node
+            for i in range(element.attributes.length):
+                an = element.attributes.item(i)
+                thisNode[an.name] = an.nodeValue
+
+            for a in element.childNodes:
+                if a.nodeType == xml.dom.Node.ELEMENT_NODE:
+
+                    child = XMLNode()
+                    try:
+                        list = getattr(thisNode, a.nodeName)
+                    except AttributeError:
+                        setattr(thisNode, a.nodeName, [])
+
+                    # add the child node as an attrib to this node
+                    list = getattr(thisNode, a.nodeName);
+                    #print "appending child: %s to %s" % (a.nodeName, thisNode.elementName)
+                    list.append(child);
+
+                    __parseXMLElement(a, child)
+
+                elif a.nodeType == xml.dom.Node.TEXT_NODE:
+                    thisNode.elementText += a.nodeValue
+
+            return thisNode
+
+        dom = parseString(xmlStr)
+
+        # get the root
+        rootNode = XMLNode()
+        if storeXML: rootNode.xml = xmlStr
+
+        return __parseXMLElement(dom.firstChild, rootNode)
+
+class BoxDotNetError(Exception):
+    """Exception class for errors received from Facebook."""
+    pass
+
+class BoxDotNet(object):
+    END_POINT = 'http://www.box.net/api/1.0/rest?'
+
+    #The box.net return status codes are all over the show
+    # method_name : return_value_that_is_ok
+    RETURN_CODES = {
+        'get_ticket'        :   'get_ticket_ok',
+        'get_auth_token'    :   'get_auth_token_ok',
+        'get_account_tree'  :   'listing_ok',
+        'logout'            :   'logout_ok',
+        'create_folder'     :   'create_ok',
+        'upload'            :   'upload_ok',
+        'delete'            :   's_delete_node'
+    }
+
+    def __init__(self, browser="firefox"):
+        self.browser = browser
+
+        self.__handlerCache={}
+
+    @classmethod
+    def __fix_args(cls, **arg):
+        for key in arg.keys():
+            if isinstance(arg[key], list):
+                arg[key] = ','.join(arg[key])
+
+                value = arg[key]
+                arg[key + '[]'] = value
+                del arg[key]
+
+        return arg
+
+    @classmethod
+    def check_errors(cls, method, xml):
+        status = xml.status[0].elementText
+
+        if status == cls.RETURN_CODES[method]:
+            return
+
+        raise BoxDotNetError ("Box.net returned [%s] for action [%s]" % (status, method))
+
+    def login(self, api_key):
+        # get ticket
+        rsp = self.get_ticket (api_key=api_key)
+        ticket = rsp.ticket[0].elementText
+        # open url
+        url = "http://www.box.net/api/1.0/auth/%s" % ticket
+        os.system('%s "%s"' % (self.browser, url))
+
+        # get token
+        rsp = self.get_auth_token(api_key=api_key, ticket=ticket)
+
+        return rsp
+
+    def __getattr__(self, method, **arg):
+        """
+        Handle all box.net calls
+        """
+        if not self.__handlerCache.has_key(method):
+            def handler(_self = self, _method = method, **arg):
+                arg = _self.__fix_args(**arg)
+
+                url = _self.END_POINT
+                arg["action"] = _method
+                postData = urllib.urlencode(arg)
+                # print "--url---------------------------------------------"
+                # print url
+                # print "--postData----------------------------------------"
+                # print postData
+                f = urllib.urlopen(url + postData)
+                data = f.read()
+                # print "--response----------------------------------------"
+                # print data
+                f.close()
+
+                xml = XMLNode.parseXML(data, True)
+                _self.check_errors(_method, xml)
+                return xml
+
+            self.__handlerCache[method] = handler;
+
+        return self.__handlerCache[method]
+
+    #-------------------------------------------------------------------
+    #-------------------------------------------------------------------
+    def upload(self, filename, **arg):
+        """
+        Upload a file to box.net.
+        """
+
+        if filename == None:
+            raise UploadException("filename OR jpegData must be specified")
+
+        # verify key names
+        for a in arg.keys():
+            if a != "api_key" and a != "auth_token" and a != "folder_id" and a != 'share':
+                sys.stderr.write("Box.net api: warning: unknown parameter \"%s\" sent to Box.net.upload\n" % (a))
+
+        url = 'http://upload.box.net/api/1.0/upload/%s/%s' % (arg['auth_token'], arg['folder_id'])
+
+        # construct POST data
+        boundary = mimetools.choose_boundary()
+        body = ""
+
+        # filename
+        body += "--%s\r\n" % (boundary)
+        body += 'Content-Disposition: form-data; name="share"\r\n\r\n'
+        body += "%s\r\n" % (arg['share'])
+
+        body += "--%s\r\n" % (boundary)
+        body += "Content-Disposition: form-data; name=\"file\";"
+        body += " filename=\"%s\"\r\n" % filename
+        body += "Content-Type: %s\r\n\r\n" % get_content_type(filename)
+
+        #print body
+
+        fp = file(filename, "rb")
+        data = fp.read()
+        fp.close()
+
+        postData = body.encode("utf_8") + data + \
+            ("\r\n--%s--" % (boundary)).encode("utf_8")
+
+        request = urllib2.Request(url)
+        request.add_data(postData)
+        request.add_header("Content-Type", \
+            "multipart/form-data; boundary=%s" % boundary)
+        response = urllib2.urlopen(request)
+        rspXML = response.read()
+
+        return XMLNode.parseXML(rspXML)
+
---
+++ conduit/dataproviders/BoxDotNetModule/BoxDotNetModule.py
@@ -0,0 +1,295 @@
+"""
+BoxDotNet Module
+"""
+import os, sys
+import gtk
+import traceback
+import md5
+
+
+import conduit
+from conduit import log,logd,logw
+import conduit.Utils as Utils
+import conduit.DataProvider as DataProvider
+import conduit.Exceptions as Exceptions
+import conduit.datatypes.File as File
+
+Utils.dataprovider_add_dir_to_path(__file__, "BoxDotNetAPI")
+from boxdotnet import BoxDotNet
+
+MODULES = {
+    "BoxDotNetSink" :          { "type": "dataprovider" }
+}
+
+class BoxDotNetSink(DataProvider.TwoWay):
+
+    _name_ = "Box.net"
+    _description_ = "Sync Your Box.net files"
+    _category_ = DataProvider.CATEGORY_FILES
+    _module_type_ = "twoway"
+    _in_type_ = "file"
+    _out_type_ = "file"
+    _icon_ = "boxdotnet"
+
+    API_KEY="nt0v6a232z6r47iftjx7g0azu6dg4p10"
+
+    def __init__(self, *args):
+        DataProvider.TwoWay.__init__(self)
+        self.need_configuration(True)
+
+        self.boxapi = None
+        self.user_id = None
+        self.token = None
+        self.foldername = ""
+        self.folder_id = None
+
+        self.files = []
+
+    #------------------------------------------
+    # File info related functions
+    #------------------------------------------
+    def _get_file_info(self, fileID):
+        """
+        Returns the id, if the id is present in the configured folder
+        """
+        files = self._get_files()
+
+        if self._get_files().count(fileID):
+            logd("File [%s] does exist" % fileID)
+            return fileID
+        else:
+            logd("File [%s] does not exist" % fileID)
+            return None
+
+    def _get_files(self):
+        """
+        Gets a list of files present in the configured folder
+        """
+        rsp = self.boxapi.get_account_tree (api_key=BoxDotNetSink.API_KEY,
+                                            auth_token=self.token,
+                                            folder_id=self.folder_id,
+                                            params=['nozip'])
+        print rsp
+        files = []
+
+        try:
+            for file in rsp.tree[0].folder[0].files[0].file:
+                files.append (file.attrib['id'])
+        finally:
+            return files
+
+    def _get_raw_file_url(self, fileID):
+        """
+        Format an url that can be used for downloading a file
+        """
+        return "http://box.net/api/1.0/download/%s/%s" % (self.token, fileID)
+
+    #------------------------------------------
+    # Upload functions
+    #------------------------------------------
+    def _upload_file (self, url, name):
+        """
+        Upload the file to url
+        """
+        rsp = self.boxapi.upload(filename=url, auth_token=self.token, folder_id=self.folder_id, share=0)
+
+        return rsp.files[0].file[0].attrib['id']
+
+    def _replace_file (self, fileID, url, name):
+        """
+        Box.net automatically replaces files with same name, so we can
+        use the plain upload method
+        """
+        return self._upload_file(url, name)
+
+    #------------------------------------------
+    # File info related functions
+    #------------------------------------------
+    def _set_login_info (self, xml):
+        """
+        Read the user id and the auth_token from the xml node
+        """
+        self.user_id = xml.user[0].user_id[0].elementText
+        self.token = xml.auth_token[0].elementText
+
+    def _get_folder_id(self):
+        """
+        Returns a folder id for the configured folder name, it re-uses existing ones
+        and creates a new folder if it isn't present
+        """
+        id = None
+
+        # see if folder already exists
+        folders = self._get_folders()
+
+        if folders.has_key (self.foldername):
+            id = folders[self.foldername]
+
+        # return if it does
+        if id:
+            return id
+        # create otherwise
+        else:
+            return self._create_folder ()
+
+    def _get_folders(self):
+        """
+        Returns a dictionary of name-id representing the upper-level
+        folders
+        """
+        rsp = self.boxapi.get_account_tree(api_key=BoxDotNetSink.API_KEY,
+                                           auth_token=self.token,
+                                           folder_id=0,
+                                           params=['nozip'])
+
+        folders = {}
+
+        try:
+            # this might throw an exception if user has no folders yet
+            for folder in rsp.tree[0].folder[0].folders[0].folder:
+                folders[folder.attrib['name']] = folder.attrib['id']
+        finally:
+            return folders
+
+    def _create_folder(self):
+        """
+        Create a top-level folder with the configured name, and return the id
+        """
+        rsp = self.boxapi.create_folder(api_key=BoxDotNetSink.API_KEY,
+                                        auth_token=self.token,
+                                        parent_id=0,
+                                        name=self.foldername,
+                                        share=0)
+
+        return rsp.folder[0].folder_id[0].elementText
+
+
+    #------------------------------------------
+    # Dataprovider Functions
+    #------------------------------------------
+    def refresh(self):
+        DataProvider.TwoWay.refresh(self)
+        self.boxapi = BoxDotNet(browser="gnome-www-browser -p")
+
+        # login if not done yet, we only login once to prevent
+        # the browser for popping up each time
+        if not self.token:
+            rsp = self.boxapi.login(BoxDotNetSink.API_KEY)
+            self._set_login_info(rsp)
+
+        # set folder id if not done yet or configuration changed
+        folder_id = self._get_folder_id()
+
+        if not self.folder_id or self.folder_id != folder_id: 
+            self.folder_id = folder_id
+
+        self.files = self._get_files()
+
+
+    def put (self, file, overwrite, LUID=None):
+        """
+        Puts the file in the sink, this uploads the file if it is not present yet
+        or updates it if necessary
+        """
+        DataProvider.TwoWay.put(self, file, overwrite, LUID)
+
+        originalName = file.get_filename()
+        #Gets the local URI (/foo/bar). If this is a remote file then
+        #it is first transferred to the local filesystem
+        fileURI = file.get_local_uri()
+
+        mimeType = file.get_mimetype()
+        
+        #Check if we have already uploaded the file
+        if LUID != None:
+            id = self._get_file_info(LUID)
+            #check if a file exists at that UID
+            if id:
+                if overwrite == True:
+                    #replace the file
+                    return self._replace_file(LUID, fileURI, originalName)
+                else:
+                    #Only upload the file if it is newer than the Remote one
+                    url = self._get_raw_file_url(id)
+                    remoteFile = File.File(url)
+
+                    #this is a limited test for equality type comparison
+                    comp = file.compare(remoteFile,True)
+                    logd("Compared %s with %s to check if they are the same (size). Result = %s" % 
+                            (file.get_filename(),remoteFile.get_filename(),comp))
+                    if comp != conduit.datatypes.COMPARISON_EQUAL:
+                        raise Exceptions.SynchronizeConflictError(comp, file, remoteFile)
+                    else:
+                        return LUID
+
+        logd("Uploading file URI = %s, Mimetype = %s, Original Name = %s" % (fileURI, mimeType, originalName))
+
+        #upload the file
+        return self._upload_file (fileURI, originalName)
+
+    def delete(self, LUID):
+        """
+        Simply call the delete method on the api
+        """
+        self.boxapi.delete (api_key=BoxDotNetSink.API_KEY, 
+                            auth_token=self.token,
+                            target='file',
+                            target_id=LUID)
+
+
+    def configure(self, window):
+        """
+        Configures the BoxDotNet sink
+        """
+        tree = Utils.dataprovider_glade_get_widget(
+                        __file__,
+                        "config.glade",
+                        "BoxDotNetConfigDialog")
+
+        #get a whole bunch of widgets
+        foldername = tree.get_widget("foldername")
+
+        #preload the widgets
+        foldername.set_text(self.foldername)
+
+        dlg = tree.get_widget("BoxDotNetConfigDialog")
+        dlg.set_transient_for(window)
+
+        response = dlg.run()
+        if response == gtk.RESPONSE_OK:
+            # get the values from the widgets
+            self.foldername = foldername.get_text()
+
+            #user must enter their username
+            self.set_configured(self.is_configured())
+
+        dlg.destroy()
+
+    def is_configured (self):
+        return len (self.foldername) > 0
+
+    def get_configuration(self):
+        return {
+            "foldername" : self.foldername
+            }
+
+    def get_UID(self):
+        return "%s-%s" % (self.user_id, self.foldername)
+
+    def get(self, LUID):
+        DataProvider.TwoWay.get(self, LUID)
+        url = self._get_raw_file_url(LUID)
+        f = File.File(
+                    URI=        url,
+                    group=      self.foldername
+                    )
+        f.set_open_URI(url)
+        f.set_UID(LUID)
+
+        return f
+                
+    def get_all(self):
+        DataProvider.TwoWay.get_all(self)
+        return self.files
+
+
