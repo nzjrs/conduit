23ef73a427f26d8537780dc464a454a8d683365e
John Carr <johncarr@src.gnome.org>
2007-08-07 22:24:34 +0000

    Ok, it can cope with Contacts and Events now...
    svn path=/trunk/; revision=746

--- conduit/dataproviders/OpensyncModule.py
+++ conduit/dataproviders/OpensyncModule.py
@@ -18,6 +18,7 @@ from conduit.DataProvider import DataSink
 from conduit.DataProvider import TwoWay
 
 import conduit.datatypes.Contact as Contact
+import conduit.datatypes.Event as Event
 
 try:
     import opensync
@@ -33,7 +34,151 @@ evo_config = """<config>
                     <tasks_path>default</tasks_path>
                 </config>"""
 
+
+class BaseDataprovider(DataProvider.TwoWay):
+    """
+    Generic dataprovider for interfacing with OpenSync plugins
+    """
+
+    _module_type_ = "twoway"
+
+    def __init__(self, *args):
+        DataProvider.TwoWay.__init__(self)
+        self.ctx = opensync.Context()
+        self.ctx.set_callback_object(Callbacks(self))
+        self.uids = None
+
+    def refresh(self):
+        DataProvider.TwoWay.refresh(self)
+        self.sink.connect(self.data, self.info, self.ctx)
+        self.uids = {}
+        self.sink.get_changes(self.data, self.info, self.ctx)
+
+    def get_all(self):
+        return list(self.uids.iterkeys())
+
+    def get(self, LUID):
+        return self.uids[LUID]
+
+    def put(self, obj, overwrite, LUID=None):
+        chg = self.object_to_change(obj)
+        
+        if overwrite == True:
+            chg.uid = LUID
+            chg.changetype = opensync.CHANGE_TYPE_MODIFIED
+        else:
+            chg.changetype = opensync.CHANGE_TYPE_ADDED
+
+        self.sink.commit_change(self.data, self.info, chg, self.ctx)
+        return chg.uid
+
+    def delete(self, LUID):
+        chg = opensync.Change()
+        chg.uid = LUID
+        chg.changetype = opensync.CHANGE_TYPE_DELETED
+        self.sink.commit_change(self.data, self.info, chg, self.ctx)
+
+    def finish(self):
+        self.uids = None
+        self.sink.disconnect(self.data, self.info, self.ctx)
+
+    def get_UID(self):
+        return "foobar"
+
+    def convert(self, data, chgfrom, chgto):
+        """ Invoke an opensync conversion plugin """
+        if chgfrom == chgto:
+            return data
+
+        formatfrom = self.formats.find_objformat(chgfrom)
+        formatto = self.formats.find_objformat(chgto)
+        converter = self.formats.find_converter(formatfrom, formatto)
+        return converter.invoke(data, "")
+
+    def change_to_object(self, change):
+        """ Map from an opensync data object to a Conduit data object """
+        raise NotImplementedError
+
+    def object_to_change(self, obj):
+        """ Map from a Conduit data object to an opensync data object """
+        raise NotImplementedError
+
+
+class Callbacks(opensync.ContextCallbacks):
+    def __init__(self, dp):
+        self.dp = dp
+
+    def callback(self, err):
+        pass
+
+    def changes(self, change):
+        self.dp.uids[change.uid] = self.dp.change_to_object(change)
+
+    def warning(self, err):
+        logd("warning: %s" % err)
+
+
+class ContactDataprovider(BaseDataprovider):
+
+    _in_type_ = "contact"
+    _out_type_ = "contact"
+    _icon_ = "contact-new"
+    _osync_type_ = "vcard30"
+
+    def change_to_object(self, change):
+        uid = change.uid
+        # FIXME: Shouldn't need to trim the data!
+        data = str(change.data.data)[:-1]
+
+        contact = Contact.Contact(None)
+        contact.set_from_vcard_string(data)
+        # contact.set_mtime(...)
+        contact.set_UID(change.uid)
+        return contact
+
+    def object_to_change(self, obj):
+        chg = opensync.Change()
+        chg.format = "vcard30"
+        chg.objtype = "data"
+        format = self.formats.find_objformat("vcard30")
+        vcard = str(obj.get_vcard_string())
+        chg.data = opensync.Data(vcard, format)
+        return chg
+
+class EventDataprovider(BaseDataprovider):
+
+    _in_type_ = "event"
+    _out_type_ = "event"
+    _icon_ = "contact-new"
+    _osync_type_ = "vevent20"
+
+    def change_to_object(self, change):
+        uid = change.uid
+        # FIXME: Shouldn't need to trim the data!
+        data = str(change.data.data)[:-1]
+        
+        event = Event.Event(None)
+        event.set_from_ical_string(data)
+        event.set_UID(uid)
+        # event.set_mtime(...)
+        return event
+
+    def object_to_change(self, obj):
+        chg = opensync.Change()
+        chg.format = "vevent20"
+        chg.objtype = "data"
+        format = self.formats.find_objformat("vevent20")
+        vcard = str(obj.get_ical_string())
+        chg.data = opensync.Data(vcard, format)
+        return chg
+
+
 class OpenSyncFactory(DataProvider.DataProviderFactory):
+    types = {
+        "contact": ContactDataprovider,
+        "event":   EventDataprovider,
+    }
+
     def __init__(self, *args, **kwargs):
         DataProvider.DataProviderFactory.__init__(self, *args, **kwargs)
 
@@ -67,144 +212,19 @@ class OpenSyncFactory(DataProvider.DataProviderFactory):
         fields = { 
             "_name_":         sink.name,
             "_description_":  plugin.description,
-            "_module_type_":  "twoway",
-            "_in_type_":      "contact",
-            "_out_type_":     "contact",
-            "_icon_":         "contact-new",
             "sink":           sink,
             "data":           data,
             "info":           info,
             "formats":        self.formats,
         }
         name = "opensync-" + plugin.name + "-" + sink.name
-        dataprovider = type(name, (ContactDataprovider, ), fields)
 
-        key = self.emit_added(
-                                  dataprovider, 
-                                  (name, ), 
-                                  category
-                             )
+        if sink.name in self.types:
+            dataprovider = type(name, (self.types[sink.name], ), fields)
 
-
-class BaseDataprovider(DataProvider.TwoWay):
-    """
-    Generic dataprovider for interfacing with OpenSync plugins
-    """
-
-    def __init__(self, *args):
-        DataProvider.TwoWay.__init__(self)
-        self.ctx = opensync.Context()
-        self.ctx.set_callback_object(Callbacks(self))
-        self.uids = None
-
-    def refresh(self):
-        DataProvider.TwoWay.refresh(self)
-        self.sink.connect(self.data, self.info, self.ctx)
-        self.uids = {}
-        self.sink.get_changes(self.data, self.info, self.ctx)
-
-    def get_all(self):
-        return list(self.uids.iterkeys())
-
-    def get(self, LUID):
-        return self.uids[LUID]
-
-    def put(self, obj, overwrite, LUID=None):
-        chg = opensync.Change()
-        if overwrite == True:
-            chg.uid = LUID
-            chg.changetype = opensync.CHANGE_TYPE_MODIFIED
-        else:
-            chg.changetype = opensync.CHANGE_TYPE_ADDED
-
-        chg.format = "vcard30"
-        chg.objtype = "data"
-
-        chg.data = self.object_to_change(obj)
-
-        self.sink.commit_change(self.data, self.info, chg, self.ctx)
-        return chg.uid
-
-    def delete(self, LUID):
-        chg = opensync.Change()
-        chg.uid = LUID
-        # change.format = "plain"
-        # change.objtype = "data"
-        chg.changetype = opensync.CHANGE_TYPE_DELETED
-        self.sink.commit_change(self.data, self.info, chg, self.ctx)
-
-    def finish(self):
-        self.uids = None
-        self.sink.disconnect(self.data, self.info, self.ctx)
-
-    def get_UID(self):
-        return "foobar"
-
-    def change_to_object(self, change):
-        """ Map from an opensync data object to a Conduit data object """
-        raise NotImplementedError
-
-    def object_to_change(self, obj):
-        """ Map from a Conduit data object to an opensync data object """
-        raise NotImplementedError
-
-
-class Callbacks(opensync.ContextCallbacks):
-    def __init__(self, dp):
-        self.dp = dp
-
-    def callback(self, err):
-        pass
-
-    def changes(self, change):
-        self.dp.uids[change.uid] = self.dp.change_to_object(change)
-
-    def warning(self, err):
-        logd("warning: %s" % err)
-
-
-class ContactDataprovider(BaseDataprovider):
-
-    _in_type_ = "contact"
-    _out_type_ = "contact"
-
-    def change_to_object(self, change):
-        change = obj.change
-        uid = change.uid
-        # FIXME: Shouldn't need to trim the data!
-        data = str(change.data.data)[:-1]
-
-        contact = Contact.Contact(None)
-        contact.set_from_vcard_string(data)
-        # contact.set_mtime(change.data.revision)
-        contact.set_UID(change.uid)
-        return contact
-
-    def object_to_change(self, obj):
-        format = self.formats.find_objformat("vcard30")
-        vcard = str(obj.get_vcard_string())
-        data = opensync.Data(vcard, format)
-
-
-class EventDataprovider(BaseDataprovider):
-
-    _in_type_ = "event"
-    _out_type_ = "event"
-
-    def change_to_object(self, change):
-        change = obj.change
-        uid = change.uid
-        # FIXME: Shouldn't need to trim the data!
-        data = str(change.data.data)[:-1]
-
-        contact = Contact.Contact(None)
-        contact.set_from_vcard_string(data)
-        # contact.set_mtime(change.data.revision)
-        contact.set_UID(change.uid)
-        return contact
-
-    def object_to_change(self, obj):
-        format = self.formats.find_objformat("vcard30")
-        vcard = str(obj.get_vcard_string())
-        data = opensync.Data(vcard, format)
+            key = self.emit_added(
+                dataprovider, 
+                (name, ), 
+                category
+            )
 
