d909167eff1a3957c0012078312af356b1666510
John Stowers <jstowers@src.gnome.org>
2007-08-20 22:47:25 +0000

    Clean up autotools stuff. Patches from Rodney Dawes and Jonny Lamb. Thanks a lot guys
    svn path=/trunk/; revision=802

--- autogen.sh
+++ autogen.sh
@@ -13,8 +13,8 @@ PKG_NAME="conduit"
 }
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome development packages"
     exit 1
 }
 
-USE_GNOME2_MACROS=1 . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
