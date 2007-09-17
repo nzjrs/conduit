057fd39ee4883167f3f8ae77d0816188a37048b6
John Stowers <jstowers@src.gnome.org>
2007-09-17 10:02:28 +0000

    Example conduit GUI remote control
    svn path=/trunk/; revision=855

---
+++ tools/conduit-gui-remote.py
@@ -0,0 +1,54 @@
+import dbus, dbus.glib
+
+# we only operate on directories
+SUPPORTED_FORMAT = 'x-directory/normal'
+
+#dbus interfaces
+APPLICATION_DBUS_IFACE='org.conduit.Application'
+DATAPROVIDER_DBUS_IFACE="org.conduit.DataProvider"
+CONDUIT_DBUS_IFACE="org.conduit.Conduit"
+SYNCSET_DBUS_IFACE="org.conduit.SyncSet"
+
+# supported dps
+FLICKR_TWOWAY="FlickrTwoWay"
+FOLDER_TWOWAY="FolderTwoWay"
+
+def get_conduit_app ():
+    bus = dbus.SessionBus()
+
+    try:
+        remote_object = bus.get_object(APPLICATION_DBUS_IFACE,"/")
+        return dbus.Interface(remote_object, APPLICATION_DBUS_IFACE)
+    except dbus.exceptions.DBusException:
+        return None
+
+if __name__ == "__main__":
+    app = get_conduit_app()
+    
+    if not app:
+        print "Check Conduit Running"
+        exit()
+
+    # check if they needed providers are available
+    dps = app.GetAllDataProviders()
+
+    if not FOLDER_TWOWAY in dps and not FLICKR_TWOWAY in dps:
+        print "Could not find folder/flickt"
+        exit()
+
+    bus = dbus.SessionBus()
+
+    # create dataproviders
+    folder_twoway_path = app.GetDataProvider(FOLDER_TWOWAY)
+    sink_path = app.GetDataProvider(FLICKR_TWOWAY)
+
+    # now create conduit
+    conduit_path = app.BuildConduit (folder_twoway_path, sink_path)
+    conduit = bus.get_object(CONDUIT_DBUS_IFACE, conduit_path)
+    
+    # syncset
+    ss = bus.get_object(CONDUIT_DBUS_IFACE, "/syncset/gui")
+    ss.AddConduit(conduit)
+    
+    
+
