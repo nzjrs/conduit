1fece5e0bb80e4fd564a3d3f3ffc4ce13ad21cfd
John Stowers <jstowers@src.gnome.org>
2007-09-28 12:33:35 +0000

    Remove dublicat Globals
    svn path=/trunk/; revision=900

--- conduit/Globals.py
+++ conduit/Globals.py
@@ -15,20 +15,6 @@ class Globals(object):
         #syncManager provides the single point of cancellation when exiting
         self.syncManager = None
 
-"""
-Excapsulates those items global to the Conduit process
-"""
-import conduit.Settings as Settings
-
-class Globals(object):
-    def __init__(self):
-        #settings is global and initialized early
-        self.settings = Settings.Settings()
-
-        #to save resources DB, moduleManager and typeConverter are global
-        self.moduleManager = None
-        self.typeConverter = None
-        self.mappingDB = None
-        #syncManager provides the single point of cancellation when exiting
-        self.syncManager = None
+        #the main application
+        self.app = None        
 
