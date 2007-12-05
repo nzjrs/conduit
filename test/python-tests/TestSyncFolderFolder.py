a58e55d6cb8411ddf6ed23fd4d78f5da6911cb1e
John Stowers <jstowers@src.gnome.org>
2007-12-05 10:03:51 +0000

    Expand the folder sync to test one way sync, and the mappingDB
    svn path=/trunk/; revision=1134

--- test/python-tests/TestSyncFolderFolder.py
+++ test/python-tests/TestSyncFolderFolder.py
@@ -9,16 +9,21 @@ import traceback
 import random
 import time
 
+#Repeat syncs a few times to check for duplicate mapping bugs, etc
+SYNC_N_TIMES = 3
+#Num files to start with, should end with 150% of this number
 NUM_FILES = 10
-GROUP = "TestGroup"
+#Sleep time for file I/O
+SLEEP_TIME = 1
+#Print the mapping DB on the last sync?
+PRINT_MAPPING_DB = True
 
+#setup test
 test = SimpleSyncTest()
-test.set_two_way_policy({"conflict":"replace","deleted":"replace"})
-
-#setup the conduit
 sourceW = test.get_dataprovider("FolderTwoWay")
 sinkW = test.get_dataprovider("FolderTwoWay")
 test.prepare(sourceW, sinkW)
+test.set_two_way_policy({"conflict":"replace","deleted":"replace"})
 
 #prepare the test data
 sourceDir = os.path.join(os.environ['TEST_DIRECTORY'],"source")
@@ -42,15 +47,9 @@ for i in range(0, NUM_FILES):
     f.transfer(dest, True)
     FILES.append((name,contents))
 
-#create the special files that signify the folders should be synced together
-for i in (sourceDir, sinkDir):
-    f = File.TempFile(GROUP)
-    f.force_new_filename(".conduit.conf")
-    f.transfer(i,True)
-
 #configure the source and sink
 config = {}
-config["folderGroupName"] = GROUP
+config["folderGroupName"] = "TestGroup"
 config["folder"] = "file://"+sourceDir
 config["includeHidden"] = False
 test.configure(source=config)
@@ -61,21 +60,42 @@ test.configure(sink=config)
 #check they scan the dirs ok
 a = test.get_source_count()
 b = test.get_sink_count()
-ok("Sync: Got all items (%s,%s,%s)" % (a,b,len(FILES)), (a+b)==len(FILES))
+ok("Refresh: Got all items (%s,%s,%s)" % (a,b,len(FILES)), (a+b)==len(FILES))
 
-#sync
+#first try a one way sync
+test.set_two_way_sync(False)
+for i in range(1,SYNC_N_TIMES+1):
+    test.sync(debug = i == SYNC_N_TIMES and PRINT_MAPPING_DB)
+    time.sleep(SLEEP_TIME)
+
+    abort,error,conflict = test.get_sync_result()
+    ok("Oneway Sync: Sync #%s completed" % i, abort == False and error == False and conflict == False)
+
+    a = test.get_source_count()
+    b = test.get_sink_count()
+    ok("Oneway Sync: Sync all items (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES)/2 and b==len(FILES))
+
+    mapSource2Sink = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sourceW.get_UID(), sinkW.get_UID())
+    mapSink2Source = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sinkW.get_UID(), sourceW.get_UID())
+    print mapSource2Sink, mapSink2Source
+    ok("Oneway Sync: 10 Mappings in total", len(mapSource2Sink) == 5 and len(mapSink2Source) == 0)
+
+#two way sync
 test.set_two_way_sync(True)
-test.sync()
-aborted = test.sync_aborted()
-ok("Sync completed", aborted == False)
+for i in range(1,SYNC_N_TIMES+1):
+    test.sync(debug = i == SYNC_N_TIMES and PRINT_MAPPING_DB)
+    time.sleep(SLEEP_TIME)
 
-#some IO time
-time.sleep(1)
+    abort,error,conflict = test.get_sync_result()
+    ok("Sync: Sync #%s completed" % i, abort == False and error == False and conflict == False)
 
-a = test.get_source_count()
-b = test.get_sink_count()
-ok("Sync: Sync all items (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES) and b==len(FILES))
+    a = test.get_source_count()
+    b = test.get_sink_count()
+    ok("Sync: Sync all items (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES) and b==len(FILES))
 
+    mapSource2Sink = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sourceW.get_UID(), sinkW.get_UID())
+    mapSink2Source = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sinkW.get_UID(), sourceW.get_UID())
+    ok("Sync: 10 Mappings in total", len(mapSource2Sink + mapSink2Source) == 10)
 
 for name,contents in FILES:
     f1 = File.File(os.path.join(sourceDir, name))
@@ -97,17 +117,25 @@ for i in range(0, NUM_FILES/2):
     os.remove(path)
 
 #some IO time
-time.sleep(1)
+time.sleep(SLEEP_TIME)
 
 #SYNC and wait for sync to finish (block)
-test.sync()
-time.sleep(1)
-aborted = test.sync_aborted()
-ok("Sync completed", aborted == False)
+for i in range(1,SYNC_N_TIMES+1):
+    test.sync(debug = i == SYNC_N_TIMES and PRINT_MAPPING_DB)
+    time.sleep(SLEEP_TIME)
+    
+    abort,error,conflict = test.get_sync_result()
+    #There will only be a conflict (delete) the first sync, because the two way policy
+    #is to replace the deleted items
+    ok("Delete: Sync #%s completed" % i, abort == False and error == False and conflict == (i == 1))
 
-a = test.get_source_count()
-b = test.get_sink_count()
-ok("Delete: Files were deleted (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES) and b==len(FILES))
+    a = test.get_source_count()
+    b = test.get_sink_count()
+    ok("Delete: Files were deleted (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES) and b==len(FILES))
+    mapSource2Sink = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sourceW.get_UID(), sinkW.get_UID())
+    mapSink2Source = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sinkW.get_UID(), sourceW.get_UID())
+    ok("Delete: 5 Mappings in total", len(mapSource2Sink + mapSink2Source) == 5)
+
 
 for name,contents in FILES:
     f1 = File.File(os.path.join(sourceDir, name))
@@ -116,7 +144,7 @@ for name,contents in FILES:
     ok("Delete: checking source/%s == sink/%s" % (name, name),comp == COMPARISON_EQUAL)
 
 #test hidden files and folders
-config["folderGroupName"] = GROUP
+config["folderGroupName"] = "TestGroup"
 config["folder"] = "file://"+sourceDir
 config["includeHidden"] = True
 test.configure(source=config)
@@ -143,14 +171,18 @@ for i in range(0, NUM_FILES):
     f.transfer(dest, True)
     FILES.append((name,contents))
 
-#SYNC and wait for sync to finish (block)
-test.sync()
-time.sleep(1)
-aborted = test.sync_aborted()
-ok("Sync completed", aborted == False)
+for i in range(1,SYNC_N_TIMES+1):
+    test.sync(debug = i == SYNC_N_TIMES and PRINT_MAPPING_DB)
+    time.sleep(SLEEP_TIME)
+    
+    abort,error,conflict = test.get_sync_result()
+    ok("Hidden: Sync #%s completed" % i, abort == False and error == False and conflict == False)
 
-a = test.get_source_count()
-b = test.get_sink_count()
-ok("Hidden: Sync all items (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES) and b==len(FILES))
+    a = test.get_source_count()
+    b = test.get_sink_count()
+    ok("Hidden: Sync all items (%s,%s,%s)" % (a,b,len(FILES)), a==len(FILES) and b==len(FILES))
+    mapSource2Sink = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sourceW.get_UID(), sinkW.get_UID())
+    mapSink2Source = conduit.GLOBALS.mappingDB.get_mappings_for_dataproviders(sinkW.get_UID(), sourceW.get_UID())
+    ok("Hidden: 15 Mappings in total", len(mapSource2Sink + mapSink2Source) == 15)
 
 finished()
