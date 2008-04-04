93d317f4f4a9080161d5befd37f5062e0d84fe50
John Stowers <john.stowers@gmail.com>
2008-04-04 11:47:52 +0000

    Dont enable network sync support on systems with < python2.5
    2008-04-05  John Stowers  <john.stowers@gmail.com>
    
    	* conduit/modules/NetworkModule/NetworkModule.py: Dont enable network 
    	sync support on systems with < python2.5
    
    
    svn path=/trunk/; revision=1409

--- conduit/modules/NetworkModule/NetworkModule.py
+++ conduit/modules/NetworkModule/NetworkModule.py
@@ -5,19 +5,30 @@ receiving python objects over the network.
 Copyright: John Stowers, 2006
 License: GPLv2
 """
-import conduit.utils as Utils
-Utils.dataprovider_add_dir_to_path(__file__, "")
+import sys
+import logging
+log = logging.getLogger("modules.Network")
 
-import Client
-import Server
+#We need Python2.5 for network sync. This is because allow_none (i.e.
+#the marshalling of None in the xmlrpc server) was only added in Python2.5
+if sys.version_info[0:2] >= (2,5):
+    import conduit.utils as Utils
+    Utils.dataprovider_add_dir_to_path(__file__, "")
 
-NetworkClientFactory = Client.NetworkClientFactory
-NetworkServerFactory = Server.NetworkServerFactory
-NetworkEndpoint = Server.NetworkEndpoint
+    import Client
+    import Server
 
-MODULES = {
-        "NetworkServerFactory" :     { "type": "dataprovider-factory" },
-        "NetworkClientFactory" :     { "type": "dataprovider-factory" },
-        "NetworkEndpoint"      :     { "type": "dataprovider" },
-}
+    NetworkClientFactory = Client.NetworkClientFactory
+    NetworkServerFactory = Server.NetworkServerFactory
+    NetworkEndpoint = Server.NetworkEndpoint
+
+    MODULES = {
+            "NetworkServerFactory" :     { "type": "dataprovider-factory" },
+            "NetworkClientFactory" :     { "type": "dataprovider-factory" },
+            "NetworkEndpoint"      :     { "type": "dataprovider" },
+    }
+    log.info("Network support enabled")
+else:
+    MODULES = {}
+    log.info("Network support disabled")
 
