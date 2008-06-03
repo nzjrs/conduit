f3fa53e2ea0056a9585d0cba156424badfcd9a0d
John Stowers <john.stowers@gmail.com>
2008-06-03 21:30:17 +0000

    Version bump to 0.3.12
    2008-06-04  John Stowers  <john.stowers@gmail.com>
    
    	* NEWS:
    	* conduit/__init__.py:
    	* configure.ac: Version bump to 0.3.12
    
    
    svn path=/trunk/; revision=1497

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,9 +6,9 @@ if [ ! -f $APP ] ; then
 fi
 
 ./scripts/maintainer.py \
-    --revision=0.3.11 \
+    --revision=0.3.11.1 \
     --package-name=Conduit \
-    --package-version=0.3.11.1 \
+    --package-version=0.3.12 \
     --package-module=conduit \
     --release-note-template=scripts/release-template.txt \
     $*
