2162241ed5deaac1c920a697a012b15558fc2817
John Stowers <john.stowers@gmail.com>
2008-08-03 11:11:31 +0000

    Post release version bump to 0.3.14
    2008-08-03  John Stowers  <john.stowers@gmail.com>
    
    	* NEWS:
    	* conduit/__init__.py:
    	* configure.ac:
    	* release.sh: Post release version bump to 0.3.14
    
    
    svn path=/trunk/; revision=1644

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,9 +6,9 @@ if [ ! -f $APP ] ; then
 fi
 
 ./scripts/maintainer.py \
-    --revision=0.3.12 \
+    --revision=0.3.13 \
     --package-name=Conduit \
-    --package-version=0.3.13 \
+    --package-version=0.3.14 \
     --package-module=conduit \
     --release-note-template=scripts/release-template.txt \
     $*
