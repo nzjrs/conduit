86d81dc10878a9c70daf6f4d6eab0c1474715ae7
John Stowers <jstowers@src.gnome.org>
2008-08-03 10:48:30 +0000

    Put the browser in a scrolled window.
    	* conduit/platform/WebBrowserWebkit.py: Put the browser in a scrolled
    	window.
    
    svn path=/trunk/; revision=1638

--- conduit/platform/WebBrowserWebkit.py
+++ conduit/platform/WebBrowserWebkit.py
@@ -1,13 +1,17 @@
+import gtk
 import webkit
 import conduit.platform
 
 class WebBrowserImpl(conduit.platform.WebBrowser):
     def __init__(self):
         conduit.platform.WebBrowser.__init__(self)
+        self.sw = gtk.ScrolledWindow()
+        self.sw.set_policy(gtk.POLICY_AUTOMATIC,gtk.POLICY_AUTOMATIC)
         self.webView = webkit.WebView()
+        self.sw.add(self.webView)
 
     def widget(self):
-        return self.webView
+        return self.sw
  
     def load_url(self,url):
         self.webView.open(url)
