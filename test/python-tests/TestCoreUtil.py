4cfbe1a9850baf387d24af9ee26cc2ae62d485f8
John Stowers <john.stowers@gmail.com>
2008-11-06 14:45:58 +0000

    Fix coreutil tests
    2008-11-06  John Stowers  <john.stowers@gmail.com>
    
    	* test/python-tests/TestCoreUtil.py: Fix coreutil tests
    
    
    svn path=/trunk/; revision=1791

--- test/python-tests/TestCoreUtil.py
+++ test/python-tests/TestCoreUtil.py
@@ -12,7 +12,7 @@ import sys
 
 if is_online():
     ts = Utils.get_http_resource_last_modified("http://files.conduit-project.org/Conduit-0.3.0-screencast-small.mpeg")
-    ok("Got mtime (timestamp) of http resource", ts == 1178419656.0)
+    ok("Got mtime (timestamp) of http resource", ts == 1178459256.0)
 
     mimetype = Utils.get_http_resource_mimetype("http://files.conduit-project.org/Conduit-0.3.0-screencast-small.mpeg")
     ok("Got mimetype of http resource", mimetype == 'video/mpeg')
@@ -63,7 +63,7 @@ s = Utils.get_user_string()
 ok("user string: %s" % s, len(s) > 0 and type(s) == str)
 
 ts = 0
-dt = datetime.datetime(1970, 1, 1, 12, 0)
+dt = datetime.datetime(1970, 1, 1, 1, 0)
 ok("Datetime to unix timestamp", Utils.datetime_get_timestamp(dt) == ts)
 ok("Unix timestamp to datetime", Utils.datetime_from_timestamp(ts) == dt)
 
