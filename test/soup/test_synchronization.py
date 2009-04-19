531ae4d5f769caa80d415f87a199e9580e6e5cab
John Carr <john.carr@unrouted.co.uk>
2009-04-19 14:09:05 +0100

    Run sync tests on *all* combinations of module wrapper

--- test/soup/test_synchronization.py
+++ test/soup/test_synchronization.py
@@ -1,3 +1,25 @@
+import unittest
 
-class TestSynchronization(BaseTest):
-    pass
+import modules
+
+class TestSynchronizationPair(BaseTest):
+
+    def __init__(self, source, sink):
+        super(TestSynchronizationPair, self).__init__(self)
+        self.source = source
+        self.sink = sink
+
+    def testDoNothing(self):
+        pass
+
+class TestSynchronization(unittest.TestSuite):
+
+    def __init__(self, tests=None):
+        """ Generate TestSuite for all module wrapper pairs  """
+        tests = []
+        for a in modules.all():
+            for b in modules.all():
+                tests.append(TestSynchronizationPair(a, b))
+
+        super(TestSynchronization, self).__init__(tests)
+
