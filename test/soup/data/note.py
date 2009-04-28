ea77477e1bfec87f416c9ea26bef9f185ef2df6e
John Carr <john.carr@unrouted.co.uk>
2009-04-28 10:15:08 -0700

    Fix test note generation

--- test/soup/data/note.py
+++ test/soup/data/note.py
@@ -8,7 +8,7 @@ class NoteWrapper(soup.data.DataWrapper):
     def iter_samples(self):
         #FIXME: This is not very useful
         for f in self.get_files_from_data_dir("*"):
-            n = Note.Note(title=Utils.random_string(), content=Utils.random_string())
+            n = Note.Note(Utils.random_string(), Utils.random_string())
             n.set_UID(Utils.random_string())
             yield n
 
