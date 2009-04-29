4dcfcacf7e5d666a6eb8de39dfdb4e8da6fddf3b
John Carr <john.carr@unrouted.co.uk>
2009-04-29 07:37:46 -0700

    Configure folder correctly, ahem

--- test/soup/modules/folder.py
+++ test/soup/modules/folder.py
@@ -11,7 +11,7 @@ class Folder(soup.modules.ModuleWrapper):
     def create_dataprovider(self):
         dp = self.conduit.get_dataprovider("FolderTwoWay")
         dp.module.set_configuration({
-            "source": Utils.new_tempdir(),
+            "folder": Utils.new_tempdir(),
         })
         return dp
 
