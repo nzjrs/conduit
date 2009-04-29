252aa7aa537eb79317bdb9153fb33860cea02077
John Carr <john.carr@unrouted.co.uk>
2009-04-29 07:35:14 -0700

    We only have test pngs, not jpgs

--- test/soup/data/photo.py
+++ test/soup/data/photo.py
@@ -5,7 +5,7 @@ from conduit.datatypes import Photo
 class PhotoWrapper(soup.data.DataWrapper):
 
     def iter_samples(self):
-        for f in self.get_files_from_data_dir("*.jpg"):
+        for f in self.get_files_from_data_dir("*.png"):
             p = Photo.Photo(URI=f)
             p.set_UID(p._get_text_uri())
             yield p
