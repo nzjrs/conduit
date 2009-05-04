8be59f51636250e3e14c76542cc6cdbf577f25df
John Carr <john.carr@unrouted.co.uk>
2009-05-04 08:36:35 -0700

    Add a progress bar

---
+++ test/soup/result.py
@@ -0,0 +1,100 @@
+
+from soup import UnavailableFeature
+from soup.utils import progressbar
+
+import sys
+import unittest
+
+class TestResult(unittest.TestResult):
+
+    def __init__(self, stream, descriptions, verbosity, num_tests=None):
+        super(TestResult, self).__init__()
+        self.stream = stream
+        self.descriptions = descriptions
+        self.verbosity = verbosity
+        self.num_tests = num_tests
+
+    def startTest(self, test):
+        super(TestResult, self).startTest(test)
+        self.report_test_start(test)
+
+    def stopTest(self, test):
+        super(TestResult, self).stopTest(test)
+        self.report_test_stop(test)
+
+    def addError(self, test, err):
+        if isinstance(err, UnavailableFeature):
+            self.addSkipped(self, test)
+        else:
+            super(TestResult, self).addError(test, err)
+            self.report_error(test)
+
+    def addFailure(self, test, err):
+        super(TestResult, self).addFailure(test, err)
+        self.report_failure(test)
+
+    def addSkipped(self, test):
+        self.report_skipped(test)
+
+    def addSuccess(self, test):
+        super(TestResult, self).addSuccess(test)
+        self.report_success(test)
+
+    # FIXME: Maybe these should be callbacks?
+
+    def report_test_start(self, test):
+        pass
+
+    def report_test_stop(self, test):
+        pass
+
+    def report_error(self, test):
+        pass
+
+    def report_failure(self, test):
+        pass
+
+    def report_skipped(self, test):
+        pass
+
+    def report_success(self, test):
+        pass
+
+
+class SimpleTestResult(TestResult):
+
+    def report_starting(self):
+        self.pb = progressbar.ProgressBar()
+        self.pb.max = self.num_tests
+
+    def report_test_start(self, test):
+        self.pb.update(self.pb.cur + 1)
+
+
+class VerboseConsoleTextResult(TestResult):
+
+    def report_test_start(self, test):
+        print test.shortDescription()
+
+
+class TestRunner(object):
+
+    def __init__(self, stream=sys.stderr, descriptions=0, verbosity=1):
+        self.stream = stream
+        self.descriptions = 0
+        self.verbosity = 0
+
+    def make_results(self, tests):
+        if self.verbosity > 1:
+            klass = VerboseConsoleTextResult
+        else:
+            klass = SimpleTestResult
+
+        return klass(self.stream, self.descriptions, self.verbosity, num_tests=tests.countTestCases())
+
+    def run(self, tests):
+        result = self.make_results(tests)
+        result.report_starting()
+        tests.run(result)
+        return result
+
