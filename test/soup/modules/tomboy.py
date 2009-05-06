b32026ae56409c78e988c084d4b392fc5d1ec997
John Carr <john.carr@unrouted.co.uk>
2009-05-06 14:41:47 -0700

    Teach it about standard services and how to start tomboy

--- test/soup/modules/tomboy.py
+++ test/soup/modules/tomboy.py
@@ -5,12 +5,15 @@ from soup.data.note import NoteWrapper
 
 import conduit.modules.TomboyModule as TomboyModule
 
+import dbus
+
 class Tomboy(soup.modules.ModuleWrapper):
 
     klass = TomboyModule.TomboyNoteTwoWay
     dataclass = NoteWrapper
 
     def create_dataprovider(self):
+        a, retval = dbus.SessionBus().start_service_by_name("org.gnome.Tomboy")
         return self.klass()
 
     def destroy_dataprovider(self):
