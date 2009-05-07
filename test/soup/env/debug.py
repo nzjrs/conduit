2f32d950c153e589b0a3dbe4fee1b500fcbca0ae
John Carr <john.carr@unrouted.co.uk>
2009-05-07 03:08:34 -0700

    Stub to run a test inside pdb

---
+++ test/soup/env/debug.py
@@ -0,0 +1,16 @@
+
+import soup
+
+import pdb
+
+class Debugger(soup.env.EnvironmentWrapper):
+
+    @classmethod
+    def enabled(cls, opts):
+        return False
+
+    def decorate_test(self, test):
+        def _(*args, **kwargs):
+            pdb.runcall(test, *args, **kwargs)
+        return _
+
