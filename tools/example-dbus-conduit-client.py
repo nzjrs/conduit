06b8107394bc1f6107922588af58fb051ff04199
John Stowers <john.stowers@gmail.com>
2009-05-20 00:00:09 +1200

    Port examples to new libconduit system

--- tools/example-dbus-conduit-client.py
+++ tools/example-dbus-conduit-client.py
@@ -1,92 +1,57 @@
 #!/usr/bin/env python
 
 import sys
-import dbus
 import getopt
 import os
+import dbus, dbus.glib
 
-APPLICATION_DBUS_IFACE="org.conduit.Application"
-SYNCSET_DBUS_IFACE="org.conduit.SyncSet"
-CONDUIT_DBUS_IFACE="org.conduit.Conduit"
-EXPORTER_DBUS_IFACE="org.conduit.Exporter"
-DATAPROVIDER_DBUS_IFACE="org.conduit.DataProvider"
+try:
+    import libconduit
+except ImportError:
+    import conduit.libconduit as libconduit
 
-def main(useGui, quitWhenFinished, exportFile):
-    bus = dbus.SessionBus()
+def main(useGui, quitWhenFinished, exportFile, startConduit):
+    app = libconduit.ConduitApplicationWrapper(
+                            conduitWrapperKlass=libconduit.ConduitWrapper,
+                            addToGui=useGui,
+                            store=False,
+                            debug=False
+                            )
+    if not app.connect_to_conduit(startConduit):
+        print "Conduit not running"
+        return 
 
-    #Create an Interface wrapper for the remote object
-    remote_object = bus.get_object(APPLICATION_DBUS_IFACE,"/")
-    app = dbus.Interface(remote_object, APPLICATION_DBUS_IFACE)
-
-    dps = app.GetAllDataProviders()
-    print "Available DPs"
-    for dp in dps:
+    print "Available Dataproviders"
+    for dp in app.get_dataproviders():
         print " * ",dp
 
-    #get a dataprovider
-    path = app.GetDataProvider("TestSource")
-    source = dbus.Interface(
-                bus.get_object(DATAPROVIDER_DBUS_IFACE,path),
-                DATAPROVIDER_DBUS_IFACE
-                )
+    #Use libconduit to create a file sync/exporter conduit
+    cond = app.build_conduit("TestSink")
+    #Add file to sync/export
+    cond.add_uri(exportFile)
+    #Sync all file sync/exporter conduits
+    app.sync()
 
-    print "Source Configuration"
-    print source.GetConfigurationXml()
-
-    #get another
-    path = app.GetDataProvider("TestSink")
-    sink = dbus.Interface(
-                bus.get_object(DATAPROVIDER_DBUS_IFACE,path),
-                DATAPROVIDER_DBUS_IFACE
-                )
-
-    #get a conduit
-    path = app.BuildConduit(source, sink)
-    cond = dbus.Interface(
-                bus.get_object(CONDUIT_DBUS_IFACE,path),
-                CONDUIT_DBUS_IFACE
-                )
-
-
-    if useGui:
-        remote_object = bus.get_object(SYNCSET_DBUS_IFACE,"/syncset/gui")
-    else:
-        remote_object = bus.get_object(SYNCSET_DBUS_IFACE,"/syncset/dbus")
-
-    ss = dbus.Interface(remote_object, SYNCSET_DBUS_IFACE)
-
-    print "Add the conduit to a SyncSet"
-    ss.AddConduit(cond)
-
-    print "Synchronize the conduit"
-    cond.Sync()
-
-    #now use the spiffy dbus iface to build a simple export conduit
-    if exportFile != None:
-        path = app.BuildExporter("TestSink")
-        exporter = bus.get_object(CONDUIT_DBUS_IFACE,path)
-        #now call into the exporter iface for the export specific tasks
-        exporter.AddData(exportFile,dbus_interface=EXPORTER_DBUS_IFACE)
-        #and the normal iface to sync
-        exporter.Sync(dbus_interface=CONDUIT_DBUS_IFACE)
-        
+    #Call a raw dbus message on the underlying application
     if quitWhenFinished:
-        app.Quit()
+        app.get_application().Quit()
 
-def usage():
-    usage = "example.py\n" + \
+def usage(me):
+    usage = "%s --export=FILE\n" + \
             "Options:\n" + \
             "\t--gui\tShow the conduit (syncset) in the GUI\n" + \
             "\t--quit\tClose conduit when finished"
-    print usage
+    print usage % me
     sys.exit(1)
 
 if __name__ == '__main__':
+    me = sys.argv[0]
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "e:gq", ["export=", "gui", "quit"])
+        opts, args = getopt.getopt(sys.argv[1:], "e:gqs", ["export=", "gui", "quit", "start"])
     except getopt.GetoptError:
-        usage()
+        usage(me)
 
+    start = False
     quit = False
     gui = False
     export = None
@@ -95,11 +60,16 @@ if __name__ == '__main__':
             gui = True
         if o in ("-q", "--quit"):
             quit = True
+        if o in ("-s", "--start"):
+            start = True
         if o in ("-e", "--export"):
             export = os.path.abspath(a)
-
-    main(
-        useGui=gui,
-        quitWhenFinished=quit,
-        exportFile=export
-        )
+        
+    if export and os.path.exists(export):
+        main(
+            useGui=gui,
+            quitWhenFinished=quit,
+            exportFile=export,
+            startConduit=start)
+    else:
+        usage(me)
