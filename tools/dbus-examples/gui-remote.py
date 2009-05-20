b882dcb71e609b86b9574ed8f8e888b5b85961c6
John Stowers <john.stowers@gmail.com>
2009-05-20 19:23:49 +1200

    Start making a example external gui for conduit

--- tools/dbus-examples/gui-remote.py
+++ tools/dbus-examples/gui-remote.py
@@ -1,54 +1,65 @@
+import gtk
+import sys
+import getopt
+import os
 import dbus, dbus.glib
 
-# we only operate on directories
-SUPPORTED_FORMAT = 'x-directory/normal'
+try:
+    import libconduit
+except ImportError:
+    import conduit.libconduit as libconduit
 
-#dbus interfaces
-APPLICATION_DBUS_IFACE='org.conduit.Application'
-DATAPROVIDER_DBUS_IFACE="org.conduit.DataProvider"
-CONDUIT_DBUS_IFACE="org.conduit.Conduit"
-SYNCSET_DBUS_IFACE="org.conduit.SyncSet"
+class UI:
+    def __init__(self):
+        self.app = libconduit.ConduitApplicationWrapper(
+                            conduitWrapperKlass=libconduit.ConduitWrapper,
+                            addToGui=True,
+                            store=False,
+                            debug=False)
 
-# supported dps
-FLICKR_TWOWAY="FlickrTwoWay"
-FOLDER_TWOWAY="FolderTwoWay"
+        w = gtk.Window()
+        vb = gtk.VBox()
+        w.add(vb)
 
-def get_conduit_app ():
-    bus = dbus.SessionBus()
+        #create UI, two combo boxes with dataproviders, and some buttons
+        self._dpmodel = gtk.ListStore(str, str)
+        self._source = self._make_combo()
+        vb.pack_start(self._source, False, False)
+        self._sink = self._make_combo()
+        vb.pack_start(self._sink, False, False)
 
-    try:
-        remote_object = bus.get_object(APPLICATION_DBUS_IFACE,"/")
-        return dbus.Interface(remote_object, APPLICATION_DBUS_IFACE)
-    except dbus.exceptions.DBusException:
-        return None
+        conn = gtk.Button("Connect")
+        conn.connect("clicked", self._on_connect)
+        vb.pack_start(conn, False, False)
+
+        conf = gtk.Button("Configure")
+        conf.connect("clicked", self._on_connect)
+        vb.pack_start(conf, False, False)
+
+        w.set_default_size(300,-1)
+        w.show_all()
+
+    def _make_combo(self):
+        cb = gtk.ComboBox(self._dpmodel)
+        cell = gtk.CellRendererText()
+        cb.pack_start(cell, True)
+        cb.add_attribute(cell, 'text', 0)
+        return cb
+
+    def _on_connect(self, btn):
+        if self.app.connect_to_conduit(startConduit=False):
+            self._dpmodel.clear()
+            for name,key in self.app.get_dataproviders().items():
+                self._dpmodel.append((name, key))
+            btn.set_label("Connected")
+
+    def _on_configure(self, btn):
+        if self.app.connected():
+            #build the dataprovider
+            pass
 
 if __name__ == "__main__":
-    app = get_conduit_app()
-    
-    if not app:
-        print "Check Conduit Running"
-        exit()
+    u = UI()
+    gtk.main()   
 
-    # check if they needed providers are available
-    dps = app.GetAllDataProviders()
-
-    if not FOLDER_TWOWAY in dps or not FLICKR_TWOWAY in dps:
-        print "Could not find folder/flickr"
-        exit()
-
-    bus = dbus.SessionBus()
-
-    # create dataproviders
-    folder_twoway_path = app.GetDataProvider(FOLDER_TWOWAY)
-    sink_path = app.GetDataProvider(FLICKR_TWOWAY)
-
-    # now create conduit
-    conduit_path = app.BuildConduit (folder_twoway_path, sink_path)
-    conduit_obj = bus.get_object(CONDUIT_DBUS_IFACE, conduit_path)
-    
-    # syncset
-    ss = bus.get_object(SYNCSET_DBUS_IFACE, "/syncset/gui")
-    ss.AddConduit(conduit_obj, dbus_interface=SYNCSET_DBUS_IFACE)
-    
-    
 
